a577d786929ba5a063045935d65aa179a05315c0
ca333 <ca333@keemail.me>
2019-03-20 22:08:54 +0100

    change name

--- lib/main.dart
+++ lib/main.dart
@@ -36,7 +36,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'KomodEX',
+        title: 'KomoDEX',
         localizationsDelegates: [
           AppLocalizationsDelegate(),
           GlobalMaterialLocalizations.delegate,
