297c0dccd65bcfd3e2dabd9f683cbedb40b83db2
theodoreetienne@gmail.com <etiennetheodore@orange.fr>
2019-06-14 16:15:08 +0200

    feat: adding sell order

---
+++ lib/screens/dex/orders/orders_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OrdersPage extends StatefulWidget {
+  @override
+  _OrdersPageState createState() => _OrdersPageState();
+}
+
+class _OrdersPageState extends State<OrdersPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
