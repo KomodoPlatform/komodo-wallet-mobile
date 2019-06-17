fa1333d8ddec165b21c2edd982cf87dff6371629
ca333 <ca333@keemail.me>
2019-06-17 10:14:34 +0200

    fix typo

--- lib/screens/portfolio/select_coins_page.dart
+++ lib/screens/portfolio/select_coins_page.dart
@@ -209,7 +209,7 @@ class _LoadingCoinState extends State<LoadingCoin> {
             } else if (snapshot.hasData && !snapshot.data.isActive) {
               return Text('Sorry, ${snapshot.data.coin.name} not available.');
             } else {
-               return Text("Starting markermaker...");
+               return Text("Connecting...");
             }
           }
         )
