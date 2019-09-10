df3e1fd1e417444c887cf909f5c957e3f38eb2ea
Kiruel <etiennetheodore@orange.fr>
2019-09-10 18:44:49 +0200

    fix: crash unlock view

--- lib/screens/authentification/unlock_wallet_page.dart
+++ lib/screens/authentification/unlock_wallet_page.dart
@@ -4,6 +4,7 @@ import 'package:komodo_dex/blocs/authenticate_bloc.dart';
 import 'package:komodo_dex/blocs/wallet_bloc.dart';
 import 'package:komodo_dex/localizations.dart';
 import 'package:komodo_dex/model/wallet.dart';
+import 'package:komodo_dex/screens/authentification/welcome_page.dart';
 import 'package:komodo_dex/screens/settings/restore_seed_page.dart';
 import 'package:komodo_dex/services/db/database.dart';
 import 'package:komodo_dex/widgets/primary_button.dart';
@@ -34,15 +35,13 @@ class _UnlockWalletPageState extends State<UnlockWalletPage> {
 
   @override
   Widget build(BuildContext context) {
-
     return WillPopScope(
       onWillPop: () async => !widget.isCreatedPin,
       child: Scaffold(
         resizeToAvoidBottomPadding: true,
         backgroundColor: Theme.of(context).backgroundColor,
         appBar: widget.isCreatedPin
-            ? 
-            AppBar(
+            ? AppBar(
                 leading: InkWell(
                     onTap: () async {
                       await DBProvider.db.deleteWallet(widget.wallet);
@@ -203,7 +202,7 @@ class _UnlockWalletPageState extends State<UnlockWalletPage> {
                             context,
                             MaterialPageRoute<dynamic>(
                                 builder: (BuildContext context) =>
-                                    RestoreSeedPage()),
+                                    const WelcomePage(isFromRestore: true,)),
                           );
                         },
                         child: Padding(
@@ -213,7 +212,6 @@ class _UnlockWalletPageState extends State<UnlockWalletPage> {
                             textAlign: TextAlign.center,
                             style: Theme.of(context).textTheme.body2.copyWith(
                                 decoration: TextDecoration.underline,
-
                                 decorationColor: Colors.white),
                           ),
                         ),
