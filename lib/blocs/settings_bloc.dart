0ea652786999f186c1b95c3f10b97eb5d1f9639b
Kiruel <etiennetheodore@orange.fr>
2019-09-18 14:58:47 +0200

    feat: language setting

--- lib/blocs/settings_bloc.dart
+++ lib/blocs/settings_bloc.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
 
+import 'package:flutter/material.dart';
+import 'package:komodo_dex/localizations.dart';
 import 'package:komodo_dex/widgets/bloc_provider.dart';
 
 SettingsBloc settingsBloc = SettingsBloc();
@@ -20,6 +22,25 @@ class SettingsBloc implements BlocBase{
   void setDeleteLoading(bool isLoading) {
     isDeleteLoading = isLoading;
     _inIsDeleteLoading.add(isDeleteLoading);
+  }
+
+    String getNameLanguage(BuildContext context, String languageCode) {
+    switch (languageCode) {
+      case 'en':
+        return AppLocalizations.of(context).englishLanguage;
+        break;
+      case 'fr':
+        return AppLocalizations.of(context).frenchLanguage;
+        break;
+      case 'de':
+        return AppLocalizations.of(context).deutscheLanguage;
+        break;
+      case 'zh':
+        return AppLocalizations.of(context).chineseLanguage;
+        break;
+      default:
+        return AppLocalizations.of(context).englishLanguage;
+    }
   }
   
 }
---
+++ lib/screens/settings/select_language_page.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:komodo_dex/blocs/main_bloc.dart';
+import 'package:komodo_dex/blocs/settings_bloc.dart';
+import 'package:komodo_dex/localizations.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SelectLanguagePage extends StatefulWidget {
+  const SelectLanguagePage({Key key, this.currentLoc}) : super(key: key);
+
+  final Locale currentLoc;
+
+  @override
+  _SelectLanguagePageState createState() => _SelectLanguagePageState();
+}
+
+class _SelectLanguagePageState extends State<SelectLanguagePage> {
+  Locale _currentLoc;
+  @override
+  void initState() {
+    super.initState();
+    _currentLoc = widget.currentLoc;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).backgroundColor,
+      appBar: AppBar(
+        title: Text(AppLocalizations.of(context).settingLanguageTitle),
+      ),
+      body: ListView(
+        children: mainBloc.supportedLocales
+            .map((Locale loc) => BuildItemLanguage(
+                  locale: loc,
+                  currentLoc: _currentLoc,
+                  onChange: (Locale loc) {
+                    setState(() {
+                      _currentLoc = loc;
+                      mainBloc.setNewLanguage(loc);
+                      SharedPreferences.getInstance()
+                          .then((SharedPreferences prefs) {
+                        prefs.setString('current_languages', loc.languageCode);
+                      });
+                    });
+                  },
+                ))
+            .toList(),
+      ),
+    );
+  }
+}
+
+class BuildItemLanguage extends StatefulWidget {
+  const BuildItemLanguage(
+      {Key key, this.locale, this.currentLoc, this.onChange})
+      : super(key: key);
+
+  final Locale locale;
+  final Locale currentLoc;
+  final Function(Locale) onChange;
+
+  @override
+  _BuildItemLanguageState createState() => _BuildItemLanguageState();
+}
+
+class _BuildItemLanguageState extends State<BuildItemLanguage> {
+  String getGenericScript(String scriptCode) {
+    switch (scriptCode) {
+      case 'Hans':
+        return ' ' + AppLocalizations.of(context).simplifiedChinese;
+        break;
+      case 'Hant':
+        return ' ' + AppLocalizations.of(context).traditionalChinese;
+        break;
+      default:
+        return '';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    String scripCode = '';
+    if (widget.locale.scriptCode != null) {
+      scripCode = widget.locale.scriptCode;
+    }
+    return ListTile(
+        onTap: () {
+          widget.onChange(widget.locale);
+        },
+        leading: Radio<Locale>(
+          value: widget.locale,
+          groupValue: widget.currentLoc,
+          onChanged: (Locale value) {
+            widget.onChange(value);
+          },
+        ),
+        title: Text(
+            settingsBloc.getNameLanguage(context, widget.locale.languageCode) +
+                getGenericScript(scripCode)));
+  }
+}
