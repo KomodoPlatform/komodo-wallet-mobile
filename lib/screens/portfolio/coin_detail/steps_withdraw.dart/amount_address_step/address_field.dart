431111fe147fe001fb0a8c06d861da604f719e7b
ca333 <ca333@keemail.me>
2019-09-23 20:58:35 +0200

    check LTC segwit address

--- lib/screens/portfolio/coin_detail/steps_withdraw.dart/amount_address_step/address_field.dart
+++ lib/screens/portfolio/coin_detail/steps_withdraw.dart/amount_address_step/address_field.dart
@@ -80,7 +80,10 @@ class _AddressFieldState extends State<AddressField> {
                     Log.println('', bs58check.encode(decoded));
                   } catch (e) {
                     Log.println('', e);
-                    if (value.length > 3 && (value.startsWith('bc1') || value.startsWith('3'))) {
+                    if (value.length > 3 &&
+                        (value.startsWith('bc1') ||
+                            value.startsWith('3') ||
+                            value.startsWith('M'))) {
                       return AppLocalizations.of(context)
                           .errorNotAValidAddressSegWit;
                     }
