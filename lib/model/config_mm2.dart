7c287622daa91781b54af021e647c425409ac67f
ca333 <ca333@keemail.me>
2020-03-21 16:54:43 +0100

    rm redundant null check

--- lib/model/config_mm2.dart
+++ lib/model/config_mm2.dart
@@ -11,27 +11,30 @@ ConfigMm2 configMm2FromJson(String str) => ConfigMm2.fromJson(json.decode(str));
 String configMm2ToJson(ConfigMm2 data) => json.encode(data.toJson());
 
 class ConfigMm2 {
-  ConfigMm2({
-    this.gui,
-    this.netid,
-    this.client,
-    this.userhome,
-    this.passphrase,
-    this.rpcPassword,
-    this.coins,
-    this.dbdir,
-  });
+  ConfigMm2(
+      {this.gui,
+      this.netid,
+      this.client,
+      this.userhome,
+      this.passphrase,
+      this.rpcPassword,
+      this.coins,
+      this.dbdir,
+      this.seednodes});
 
   factory ConfigMm2.fromJson(Map<String, dynamic> json) => ConfigMm2(
-        gui: json['gui'],
-        netid: json['netid'],
-        client: json['client'],
-        userhome: json['userhome'],
-        passphrase: json['passphrase'],
-        rpcPassword: json['rpc_password'],
-        coins: List<CoinInit>.from(json['coins'].map<dynamic>((dynamic x) => CoinInit.fromJson(x))) ?? <CoinInit>[],
-        dbdir: json['dbdir'],
-      );
+      gui: json['gui'],
+      netid: json['netid'],
+      client: json['client'],
+      userhome: json['userhome'],
+      passphrase: json['passphrase'],
+      rpcPassword: json['rpc_password'],
+      coins: List<CoinInit>.from(json['coins']
+              .map<dynamic>((dynamic x) => CoinInit.fromJson(x))) ??
+          <CoinInit>[],
+      dbdir: json['dbdir'],
+      seednodes: List<String>.from(json['seednodes'].map((dynamic x) => x)) ??
+          <String>[]);
 
   String gui;
   int netid;
@@ -41,6 +44,7 @@ class ConfigMm2 {
   String rpcPassword;
   List<CoinInit> coins;
   String dbdir;
+  List<String> seednodes;
 
   Map<String, dynamic> toJson() => <String, dynamic>{
         'gui': gui,
@@ -49,7 +53,12 @@ class ConfigMm2 {
         'userhome': userhome,
         'passphrase': passphrase,
         'rpc_password': rpcPassword,
-        'coins': List<dynamic>.from(coins.map<dynamic>((dynamic x) => x.toJson())) ?? <CoinInit>[],
+        'coins':
+            List<dynamic>.from(coins.map<dynamic>((dynamic x) => x.toJson())) ??
+                <CoinInit>[],
         'dbdir': dbdir,
+        'seednodes': seednodes == null
+            ? null
+            : List<dynamic>.from(seednodes.map<dynamic>((String x) => x))
       };
 }
