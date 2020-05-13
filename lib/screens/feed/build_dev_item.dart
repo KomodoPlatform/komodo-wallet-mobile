3fa658ace6f50f558964f5764c1f3cafae0539ed
yurii <yuriikhiznichenko@gmail.com>
2020-05-13 13:22:06 +0200

    WIP: Build devops list

--- lib/screens/feed/build_dev_item.dart
+++ lib/screens/feed/build_dev_item.dart
@@ -1,19 +1,287 @@
 import 'package:flutter/material.dart';
 import 'package:komodo_dex/screens/feed/devops_tab.dart';
 
-class BuildDevItem extends StatelessWidget {
+class BuildDevItem extends StatefulWidget {
   const BuildDevItem(this.dev);
 
   final Dev dev;
 
   @override
+  _BuildDevItemState createState() => _BuildDevItemState();
+}
+
+class _BuildDevItemState extends State<BuildDevItem> {
+  bool _isOpen = false;
+
+  @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: CircleAvatar(
-        backgroundImage: NetworkImage(dev.avatar),
+    return Material(
+      color: _isOpen
+          ? Theme.of(context).backgroundColor
+          : Theme.of(context).primaryColor,
+      child: Container(
+        decoration: BoxDecoration(
+            border: Border(
+                bottom: BorderSide(
+          width: 1,
+          color: _isOpen ? Theme.of(context).primaryColor : Colors.transparent,
+        ))),
+        child: Column(
+          children: <Widget>[
+            InkWell(
+              onTap: () {
+                setState(() {
+                  _isOpen = !_isOpen;
+                });
+              },
+              child: Container(
+                padding: EdgeInsets.all(12),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    CircleAvatar(
+                      backgroundImage: widget.dev.image != null
+                          ? NetworkImage(widget.dev.image)
+                          : null,
+                      backgroundColor: Theme.of(context).disabledColor,
+                      child: widget.dev.image == null
+                          ? Icon(
+                              Icons.account_circle,
+                              size: 40,
+                            )
+                          : null,
+                    ),
+                    const SizedBox(width: 12),
+                    Expanded(
+                      child: Container(
+                          child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(widget.dev.name),
+                          const SizedBox(height: 4),
+                          _buildCurrentStatus(),
+                          const SizedBox(height: 4),
+                          _buildCurrentIssue(),
+                        ],
+                      )),
+                    ),
+                    const SizedBox(width: 12),
+                    _buildOnlineStatus(),
+                  ],
+                ),
+              ),
+            ),
+            _buildDetails(),
+          ],
+        ),
       ),
-      title: Text(dev.name),
-      subtitle: Container(),
     );
+  }
+
+  Widget _buildDetails() {
+    if (!_isOpen) return Container();
+
+    final DevStatus _latest = _getLatestStatus();
+    final _buttonsList = <Widget>[
+      _buildDetailsButton(
+        iconData: Icons.thumb_up,
+        title: 'React',
+        onTap: () {},
+      ),
+      _buildDetailsButton(
+        iconData: Icons.attach_money,
+        title: 'Tip',
+        onTap: () {},
+      ),
+      if (_latest?.issue != null)
+        _buildDetailsButton(
+          iconData: Icons.error_outline,
+          title: 'Issue',
+          onTap: () {},
+        ),
+      _buildDetailsButton(
+        iconData: Icons.list,
+        title: 'Activity',
+        onTap: () {},
+      ),
+    ];
+
+    return Container(
+      child: Flex(
+        direction: Axis.horizontal,
+        children: _buttonsList,
+      ),
+    );
+  }
+
+  Widget _buildDetailsButton({
+    @required IconData iconData,
+    @required String title,
+    @required Function onTap,
+  }) {
+    return Expanded(
+      child: InkWell(
+        onTap: onTap,
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            children: <Widget>[
+              Icon(iconData, size: 20),
+              const SizedBox(
+                height: 4,
+              ),
+              Text(title, style: const TextStyle(fontSize: 13)),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCurrentStatus() {
+    final String _message = _getCurrentStatusMessage();
+    if (_message.isEmpty) return Container();
+
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Container(
+            padding: const EdgeInsets.only(right: 5, top: 1),
+            child: Icon(
+              Icons.playlist_add_check,
+              size: 15,
+              color: Theme.of(context).textTheme.caption.color,
+            )),
+        Expanded(
+          child: Text(
+            _message,
+            overflow: _isOpen ? null : TextOverflow.ellipsis,
+            style: TextStyle(
+              fontSize: 13,
+              color: Theme.of(context).textTheme.caption.color,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildCurrentIssue() {
+    final DevStatus _latest = _getLatestStatus();
+    if (_latest?.issue == null) return Container();
+
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Container(
+            padding: const EdgeInsets.only(right: 5, top: 1),
+            child: Icon(
+              Icons.error_outline,
+              size: 15,
+              color: Theme.of(context).textTheme.caption.color,
+            )),
+        Expanded(
+          child: Text(
+            _latest.issue.title,
+            overflow: _isOpen ? null : TextOverflow.ellipsis,
+            style: TextStyle(
+              fontSize: 13,
+              color: Theme.of(context).textTheme.caption.color,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildOnlineStatus() {
+    Color color;
+    Color borderColor;
+
+    switch (_getOnlineStatus()) {
+      case OnlineStatus.active:
+        {
+          color = Colors.green;
+          borderColor = Colors.green;
+          break;
+        }
+      case OnlineStatus.inactive:
+        {
+          color = const Color.fromRGBO(201, 201, 201, 1);
+          borderColor = const Color.fromRGBO(201, 201, 201, 1);
+          break;
+        }
+      case OnlineStatus.unknown:
+        {
+          color = Colors.transparent;
+          borderColor = const Color.fromRGBO(201, 201, 201, 1);
+          break;
+        }
+    }
+
+    return Container(
+      width: 10,
+      height: 10,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: color,
+        border: Border.all(width: 1, color: borderColor),
+      ),
+    );
+  }
+
+  OnlineStatus _getOnlineStatus() {
+    final DevStatus _latest = _getLatestStatus();
+    if (_latest == null) return OnlineStatus.unknown;
+
+    switch (_latest.endTime) {
+      case null:
+        {
+          return OnlineStatus.active;
+        }
+      default:
+        {
+          return OnlineStatus.inactive;
+        }
+    }
+  }
+
+  String _getCurrentStatusMessage() {
+    final DevStatus _latest = _getLatestStatus();
+    if (_latest == null) return '';
+
+    switch (_latest.endTime) {
+      case null:
+        {
+          return _latest.message ?? 'Active now'; // TODO(yurii): localization
+        }
+      default:
+        {
+          return 'Last active: ${DateTime.fromMillisecondsSinceEpoch(_latest.endTime)}'; // TODO(yurii): localization
+        }
+    }
+  }
+
+  DevStatus _getLatestStatus() {
+    final List<DevStatus> _sortedHistory =
+        _getSortedHistory(widget.dev.statusHistory);
+
+    if (_sortedHistory == null || _sortedHistory.isEmpty) return null;
+
+    return _sortedHistory[0];
+  }
+
+  List<DevStatus> _getSortedHistory(List<DevStatus> history) {
+    final List<DevStatus> _sorted = history;
+    if (_sorted == null) return null;
+    if (_sorted.isEmpty) return _sorted;
+
+    _sorted.sort((a, b) {
+      if (a.startTime < b.startTime) return 1;
+      if (a.startTime > b.startTime) return -1;
+      return 0;
+    });
+
+    return _sorted;
   }
 }
--- lib/screens/feed/devops_tab.dart
+++ lib/screens/feed/devops_tab.dart
@@ -22,23 +22,149 @@ class _DevOpsTabState extends State<DevOpsTab> {
   List<Dev> _getDevOps() {
     return [
       Dev(
-        name: 'artemciy_dice',
-        avatar: 'https://cdn.discordapp.com/avatars/209024368968204288/4ebef1887d9c9e94034387846711ff35.png?size=128',
+        id: '0',
+        name: 'ArtemGr',
+        image: 'https://avatars2.githubusercontent.com/u/41847?s=460&v=4',
+        statusHistory: [],
       ),
       Dev(
-        name: 'artemciy_dice',
-        avatar: 'https://cdn.discordapp.com/avatars/209024368968204288/4ebef1887d9c9e94034387846711ff35.png?size=128',
+        id: '1',
+        name: 'tonymorony',
+        //avatar: 'https://avatars0.githubusercontent.com/u/24797699?s=460&v=4',
       ),
+      Dev(
+        id: '2',
+        name: 'MateusRodCosta',
+        image: 'https://avatars1.githubusercontent.com/u/24463334?s=460&v=4',
+      ),
+      Dev(
+          id: '3',
+          name: 'yurii',
+          image: 'https://avatars3.githubusercontent.com/u/29194552?s=150&v=4',
+          statusHistory: [
+            DevStatus(
+              id: '0',
+              message: 'git',
+              startTime: 1588922092000,
+              endTime: 1588923652000,
+            ),
+            DevStatus(
+                id: '1',
+                message: '#757 orders duplicates',
+                startTime: 1588923772000,
+                endTime: 1588927072000,
+                issue: Issue(
+                  id: '757',
+                  title: 'Orderbook page creating orders duplicates',
+                  url: 'https://github.com/ca333/komodoDEX/issues/757',
+                )),
+            DevStatus(
+                id: '2',
+                message: 'news, design and layout, \'DevOps\' tab',
+                startTime: 1588927072000,
+                issue: Issue(
+                  id: '701',
+                  title:
+                      'rewamp the news section rewamp',
+                  url: 'https://github.com/ca333/komodoDEX/issues/701',
+                )),
+          ]),
+      Dev(
+        id: '0',
+        name: 'ArtemGr',
+        image: 'https://avatars2.githubusercontent.com/u/41847?s=460&v=4',
+        statusHistory: [],
+      ),
+      Dev(
+        id: '1',
+        name: 'tonymorony',
+        //avatar: 'https://avatars0.githubusercontent.com/u/24797699?s=460&v=4',
+      ),
+      Dev(
+        id: '2',
+        name: 'MateusRodCosta',
+        image: 'https://avatars1.githubusercontent.com/u/24463334?s=460&v=4',
+      ),
+      Dev(
+          id: '3',
+          name: 'yurii',
+          image: 'https://avatars3.githubusercontent.com/u/29194552?s=150&v=4',
+          statusHistory: [
+            DevStatus(
+              id: '0',
+              message: 'git',
+              startTime: 1588922092000,
+              endTime: 1588923652000,
+            ),
+            DevStatus(
+                id: '1',
+                message: '#757 orders duplicates',
+                startTime: 1588923772000,
+                endTime: 1588927072000,
+                issue: Issue(
+                  id: '757',
+                  title: 'Orderbook page creating orders duplicates',
+                  url: 'https://github.com/ca333/komodoDEX/issues/757',
+                )),
+            DevStatus(
+                id: '2',
+                message: 'news, design and layout, \'DevOps\' tab',
+                startTime: 1588927072000,
+                issue: Issue(
+                  id: '701',
+                  title:
+                      'rewamp the news section rewamp the news section rewamp the news section rewamp the news section rewamp the news section rewamp the news section rewamp the news section rewamp the news section rewamp the news section rewamp the news section ',
+                  url: 'https://github.com/ca333/komodoDEX/issues/701',
+                )),
+          ]),
     ];
   }
 }
 
 class Dev {
   Dev({
-    @required this.name,
-    this.avatar,
+    @required this.id,
+    this.name,
+    this.image,
+    this.statusHistory,
   });
 
+  String id;
   String name;
-  String avatar;
+  String image;
+  List<DevStatus> statusHistory;
+}
+
+class DevStatus {
+  DevStatus({
+    @required this.id,
+    this.message,
+    this.issue,
+    this.startTime,
+    this.endTime,
+  });
+
+  String id;
+  String message;
+  Issue issue;
+  int startTime;
+  int endTime;
+}
+
+class Issue {
+  Issue({
+    @required this.id,
+    this.title,
+    this.url,
+  });
+
+  String id;
+  String title;
+  String url;
+}
+
+enum OnlineStatus {
+  active,
+  inactive,
+  unknown,
 }
