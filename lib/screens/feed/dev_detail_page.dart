e339b30da64c4bc48fce3e4fdef75860e25d5975
yurii <yuriikhiznichenko@gmail.com>
2020-05-15 11:34:55 +0200

    Add current activity status to dev activity title

--- lib/screens/feed/dev_detail_page.dart
+++ lib/screens/feed/dev_detail_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:komodo_dex/screens/feed/build_dev_avatar.dart';
 import 'package:komodo_dex/screens/feed/dev.dart';
 import 'package:komodo_dex/screens/feed/dev_activity_list.dart';
+import 'package:komodo_dex/utils/utils.dart';
 
 class DevDetailsPage extends StatelessWidget {
   const DevDetailsPage({
@@ -19,7 +20,14 @@ class DevDetailsPage extends StatelessWidget {
           children: <Widget>[
             BuildDevAvatar(dev, size: 35),
             const SizedBox(width: 12),
-            Text(dev.name),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(dev.name),
+                const SizedBox(height: 2),
+                _buildCurrentStatus(context),
+              ],
+            ),
           ],
         ),
       ),
@@ -29,5 +37,29 @@ class DevDetailsPage extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  Widget _buildCurrentStatus(BuildContext context) {
+    final TextStyle _textStyle = TextStyle(
+      color: Theme.of(context).highlightColor,
+      fontWeight: FontWeight.normal,
+      fontSize: 13,
+    );
+
+    switch (dev.onlineStatus) {
+      case OnlineStatus.active:
+        {
+          return Text('Active now', style: _textStyle);
+        }
+      case OnlineStatus.inactive:
+        {
+          return Text(
+            'Last active: ${humanDate(dev.latestStatus.endTime)}',
+            style: _textStyle,
+          );
+        }
+      default:
+        return Container();
+    }
   }
 }
