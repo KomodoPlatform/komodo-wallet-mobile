94e5dcc974f9bf32c1253a125dc9513a956d176f
Mateus Rodrigues Costa <mateusrodcosta@gmail.com>
2020-05-26 18:50:51 -0300

    Attempt to fix gwei-ETH conversion

--- lib/screens/portfolio/coin_detail/steps_withdraw.dart/build_confirmation_step.dart
+++ lib/screens/portfolio/coin_detail/steps_withdraw.dart/build_confirmation_step.dart
@@ -87,7 +87,7 @@ class _BuildConfirmationStepState extends State<BuildConfirmationStep> {
           if (coinsDetailBloc.customFee != null &&
               widget.coinBalance.coin.type == 'erc') {
             fee = deci(coinsDetailBloc.customFee.gas) *
-                deci(coinsDetailBloc.customFee.gasPrice);
+                deci(coinsDetailBloc.customFee.gasPrice) / deci(1000000000);
           }
 
           Decimal amountToPay = deci(widget.amountToPay);
