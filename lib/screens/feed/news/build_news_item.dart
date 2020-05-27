fb04cd42ad8e3b746dd5b86c28361ae8b6567e78
yurii <yuriikhiznichenko@gmail.com>
2020-05-27 16:17:54 +0200

    Parse news links and handle link taps

--- lib/screens/feed/news/build_news_item.dart
+++ lib/screens/feed/news/build_news_item.dart
@@ -1,16 +1,36 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:komodo_dex/model/feed_provider.dart';
 import 'package:komodo_dex/utils/utils.dart';
 
-class BuildNewsItem extends StatelessWidget {
+class BuildNewsItem extends StatefulWidget {
   const BuildNewsItem(this.newsItem);
 
   final NewsItem newsItem;
 
   @override
+  _BuildNewsItemState createState() => _BuildNewsItemState();
+}
+
+class _BuildNewsItemState extends State<BuildNewsItem> {
+  final List<TapGestureRecognizer> _recognizers = [];
+  final String _urlMatcher =
+      r'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)';
+  final String _urlWrap = '%%';
+
+  @override
+  void dispose() {
+    for (TapGestureRecognizer recognizer in _recognizers) {
+      recognizer.dispose();
+    }
+    
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     final String _date =
-        humanDate(DateTime.parse(newsItem.date).millisecondsSinceEpoch);
+        humanDate(DateTime.parse(widget.newsItem.date).millisecondsSinceEpoch);
 
     return Container(
       padding: const EdgeInsets.all(12),
@@ -25,15 +45,43 @@ class BuildNewsItem extends StatelessWidget {
             ),
           ),
           const SizedBox(height: 10),
-          Text(
-            newsItem.content,
-            style: const TextStyle(
-              fontSize: 15,
-              height: 1.4,
-            ),
-          ),
+          _buildContent(),
         ],
       ),
+    );
+  }
+
+  Widget _buildContent() {
+    if (widget.newsItem.content == null || widget.newsItem.content.isEmpty) {
+      return Container();
+    }
+
+    final String _urlWrapped = widget.newsItem.content.replaceAllMapped(
+        RegExp(_urlMatcher), (match) => '$_urlWrap${match.group(0)}$_urlWrap');
+
+    final List<String> _chunks = _urlWrapped.split(_urlWrap);
+
+    final List<TextSpan> _spans = [];
+    for (String _chunk in _chunks) {
+      if (RegExp(_urlMatcher).hasMatch(_chunk)) {
+        final TapGestureRecognizer _recognizer = TapGestureRecognizer()
+          ..onTap = () => launchURL(_chunk);
+        _recognizers.add(_recognizer);
+
+        _spans.add(TextSpan(
+          text: '$_chunk',
+          style: TextStyle(color: Colors.blue, fontSize: 16),
+          recognizer: _recognizer,
+        ));
+      } else {
+        _spans.add(TextSpan(
+          text: '$_chunk',
+          style: const TextStyle(fontSize: 16),
+        ));
+      }
+    }
+    return RichText(
+      text: TextSpan(children: _spans),
     );
   }
 }
