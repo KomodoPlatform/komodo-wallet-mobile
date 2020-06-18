beaf4b6475f081adc84f105b8477ff6ebbf3afd7
yurii <yuriikhiznichenko@gmail.com>
2020-06-18 15:18:48 +0200

    Add news local cache

--- lib/model/feed_provider.dart
+++ lib/model/feed_provider.dart
@@ -3,11 +3,14 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'package:komodo_dex/utils/log.dart';
 import 'package:komodo_dex/utils/utils.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class FeedProvider extends ChangeNotifier {
   FeedProvider() {
-    ticker = Timer.periodic(const Duration(milliseconds: 10000), (_) {
+    _updateData();
+    ticker = Timer.periodic(const Duration(minutes: 5), (_) {
       _updateData();
     });
   }
@@ -31,32 +34,66 @@ class FeedProvider extends ChangeNotifier {
   }
 
   List<NewsItem> getNews() => _news;
+  Future<String> updateNews() => _updateNews();
 
-  Future<void> _updateData() async {
-    if (_devOps != devOpsListPlaceholder) {
-      _devOps = devOpsListPlaceholder;
-      notifyListeners();
-    }
-    if (_issues != issuesListPlaceholder) {
-      _issues = issuesListPlaceholder;
-      notifyListeners();
-    }
-
+  /// If news was successfully fetched and proceed,
+  /// updates _news, local cache, and returns 'ok'.
+  /// If failed - returns error message string
+  Future<String> _updateNews() async {
     http.Response response;
     try {
-      response = await http.get('http://95.217.44.58:777/messages');
+      response = await http.get(
+          'http://95.217.26.153/messages'); // TODO(yurii): change to domain name after DNS being updated
     } catch (e) {
-      print(e);
+      Log('feed_provider:44', '_updateNews] $e');
     }
-    final dynamic newsJson = jsonDecode(response.body);
-    _news = [];
-    for (dynamic item in newsJson) {
-      _news.add(NewsItem(
+    if (response?.statusCode != 200) {
+      return 'Unable to get news'; // TODO(yurii): localization
+    }
+
+    List<NewsItem> news;
+    try {
+      news = _newsFromJson(response.body);
+    } catch (e) {
+      Log('feed_provider:52', '_updateNews] $e');
+    }
+    if (news == null) {
+      return 'Unable to proceed news'; // TODO(yurii): localization
+    }
+
+    _news = news;
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString('cachedNews', response.body);
+
+    return 'ok';
+  }
+
+  Future<void> _updateData() async {
+    if (_news == null || _news.isEmpty) {
+      final SharedPreferences prefs = await SharedPreferences.getInstance();
+      final String cachedNews = prefs.getString('cachedNews');
+      if (cachedNews != null) {
+        _news = _newsFromJson(cachedNews);
+        notifyListeners();
+      }
+    }
+
+    if (await _updateNews() == 'ok') notifyListeners();
+  }
+
+  List<NewsItem> _newsFromJson(String body) {
+    final List<dynamic> json = jsonDecode(body);
+    List<NewsItem> news;
+    for (dynamic item in json) {
+      final String content = item['content'];
+      if (content == null || content.isEmpty) continue;
+      news ??= [];
+      news.add(NewsItem(
         date: item['date'],
-        content: item['content'],
+        content: content,
       ));
     }
-    notifyListeners();
+    return news;
   }
 
   List<DevStatus> sortTimeLine(List<DevStatus> unsorted) {
