1d92179cec04fd9ed26f54d3460d6d39b9f0128c
yurii <yuriikhiznichenko@gmail.com>
2020-06-18 10:27:15 +0200

    WIP: Test news json format

--- lib/model/feed_provider.dart
+++ lib/model/feed_provider.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:komodo_dex/utils/utils.dart';
@@ -39,9 +40,14 @@ class FeedProvider extends ChangeNotifier {
       _issues = issuesListPlaceholder;
       notifyListeners();
     }
-    if (_news != newsPlaceholder) {
-      _news = newsPlaceholder;
-      notifyListeners();
+
+    final dynamic newsJson = jsonDecode(newsPlaceholder);
+    _news = [];
+    for (dynamic item in newsJson) {
+      _news.add(NewsItem(
+        date: item['date'],
+        content: item['content'],
+      ));
     }
   }
 
@@ -178,7 +184,7 @@ class NewsItem {
 
 class NewsSource {
   NewsSource({this.name, this.url, this.pic});
-  
+
   String name;
   String url;
   String pic;
@@ -299,240 +305,6 @@ List<Issue> issuesListPlaceholder = [
   ),
 ];
 
-List<NewsItem> newsPlaceholder = [
-  NewsItem(
-    date: '2020-05-25T08:17:53.312+00:00',
-    content:
-        '''@everyone \n\nWe have published another article from our Blockchain Fundamentals series. 📚\n\n**What’s A Merkle Tree? A Simple Guide To Merkle Trees**\n\nIf you’re involved in the world of blockchain, you may have come across the phrase \"merkle tree\" before. While Merkle trees are not a widely-understood concept, they’re also not terribly complicated. This post will explain Merkle trees in plain English and help you understand how they make blockchain technology possible.\n\nRead more here.\nhttps://komodoplatform.com/whats-merkle-tree/''',
-  ),
-  NewsItem(
-    date: '2020-05-25T08:17:53.312+00:00',
-    content: '''
-    *italics* or _italics_	
-    __*underline italics*__
-    **bold**	
-    __**underline bold**__
-    ***bold italics***	
-    __***underline bold italics***__
-    __underline__	
-    ~~Strikethrough~~
 
-    test h**e**r**e**
-
-    link:  **https://www.google.com/search?q=escape_me_please**
-    ''',
-  ),
-  NewsItem(
-    date: '2020-05-25T08:17:53.312+00:00',
-    content: '''Komodo v0.6.0 Upgrade Is Coming On June 14
-
-Komodo’s fourth annual Notary Node Election came to an end on May 4, 2020, and the Komodo team would like to congratulate and welcome all of the operators who won a seat in the Notary Node network. 
-
-To officially bring these new Notary Node operators into the network, an upgrade to the Komodo daemon to version 0.6.0 will be activated on June 14, 2020 at block height 1,922,000. The upgrade is mandatory for all miners, exchanges, wallet providers, and other individuals running a full node. Please be sure to update prior to June 14.
-
-Read more here.
-https://komodoplatform.com/komodo-v0-6-0-upgrade/''',
-  ),
-  NewsItem(
-    date: '2020-05-25T08:17:53.312+00:00',
-    content: '''@everyone 
-
-Weekly Update - May 22, 2020
-
-It's Friday again, which means it's time for another weekly wrap-up. We have summarized the most important events for the last 7 days.
-
-Read the weekly update here.
-https://community.komodoplatform.com/post/5ec7e12b7ef3b37cbbaa285f''',
-  ),
-  NewsItem(
-    date: '2020-05-22T08:17:53.312+00:00',
-    content: '''Happy Bitcoin Pizza Day @everyone :pizza: 
-
-To honor the 10th anniversary of the Bitcoin Pizza Day we are running a microbounty campaign!
-
-All you need to do to win your 15 KMD is tweet out a pic of your pizza (a slice or a whole pie— both work!) with an AtomicDEX logo visible in the photo. It can be your phone with the AtomicDEX app open, a screen with the AtomicDEX homepage open, or anything else that clearly shows the AtomicDEX logo. 
-
-Get creative! The 3 coolest photos, as judged by the Komodo team, will get a small bonus in KMD.
-
-You must also tag @AtomicDEX twitter account in your tweet.
-
-The first 50 participants will each win 15 KMD! After that, no prizes will be awarded, so grab a pizza and tweet your pic as soon as possible.
-
-https://twitter.com/AtomicDex/status/1263771506175283201?s=20''',
-  ),
-  NewsItem(content: '''Emojis corrupted on Android
-    
-👨‍🦯	:man_with_probing_cane:
-👨‍🦼	:man_in_motorized_wheelchair:
-👨‍🦽	:man_in_manual_wheelchair:
-👩‍🦯	:woman_with_probing_cane:
-👩‍🦼	:woman_in_motorized_wheelchair:
-👩‍🦽	:woman_in_manual_wheelchair:
-🛕	:hindu_temple:
-🛺	:auto_rickshaw:
-🟠	:large_orange_circle:
-🟡	:large_yellow_circle:
-🟢	:large_green_circle:
-🟣	:large_purple_circle:
-🟤	:large_brown_circle:
-🟥	:large_red_square:
-🟦	:large_blue_square:
-🟧	:large_orange_square:
-🟨	:large_yellow_square:
-🟩	:large_green_square:
-🟪	:large_purple_square:
-🟫	:large_brown_square:
-🤍	:white_heart:
-🤎	:brown_heart:
-🤏	:pinching_hand:
-🤿	:diving_mask:
-🥱	:yawning_face:
-🥻	:sari:
-🦥	:sloth:
-🦦	:otter:
-🦧	:orangutan:
-🦨	:skunk:
-🦩	:flamingo:
-🦪	:oyster:
-🦮	:guide_dog:
-🦯	:probing_cane:
-🦺	:safety_vest:
-🦻	:ear_with_hearing_aid:
-🦼	:motorized_wheelchair:
-🦽	:manual_wheelchair:
-🦾	:mechanical_arm:
-🦿	:mechanical_leg:
-🧃	:beverage_box:
-🧄	:garlic:
-🧅	:onion:
-🧆	:falafel:
-🧇	:waffle:
-🧈	:butter:
-🧉	:mate_drink:
-🧊	:ice_cube:
-🧍‍♀️	:woman_standing:
-🧍‍♂️	:man_standing:
-🧍	:standing_person:
-🧎‍♀️	:woman_kneeling:
-🧎‍♂️	:man_kneeling:
-🧎	:kneeling_person:
-🧏‍♀️	:deaf_woman:
-🧏‍♂️	:deaf_man:
-🧏	:deaf_person:
-🧑‍🦯	:person_with_probing_cane:
-🧑‍🦼	:person_in_motorized_wheelchair:
-🧑‍🦽	:person_in_manual_wheelchair:
-🩰	:ballet_shoes:
-🩱	:one-piece_swimsuit:
-🩲	:briefs:
-🩳	:shorts:
-🩸	:drop_of_blood:
-🩹	:adhesive_bandage:
-🩺	:stethoscope:
-🪀	:yo-yo:
-🪁	:kite:
-🪂	:parachute:
-🪐	:ringed_planet:
-🪑	:chair:
-🪒	:razor:
-🪓	:axe:
-🪔	:diya_lamp:
-🪕	:banjo:
-    '''),
-  NewsItem(content: '''Emojis corrupted on iOs:
-
-#️⃣	:hash:
-*️⃣	:keycap_star:
-0️⃣	:zero:
-1️⃣	:one:
-2️⃣	:two:
-3️⃣	:three:
-4️⃣	:four:
-5️⃣	:five:
-6️⃣	:six:
-7️⃣	:seven:
-8️⃣	:eight:
-9️⃣	:nine:
-👨‍🦯	:man_with_probing_cane:
-👨‍🦼	:man_in_motorized_wheelchair:
-👨‍🦽	:man_in_manual_wheelchair:
-👩‍🦯	:woman_with_probing_cane:
-👩‍🦼	:woman_in_motorized_wheelchair:
-👩‍🦽	:woman_in_manual_wheelchair:
-🛕	:hindu_temple:
-🛺	:auto_rickshaw:
-🟠	:large_orange_circle:
-🟡	:large_yellow_circle:
-🟢	:large_green_circle:
-🟣	:large_purple_circle:
-🟤	:large_brown_circle:
-🟥	:large_red_square:
-🟦	:large_blue_square:
-🟧	:large_orange_square:
-🟨	:large_yellow_square:
-🟩	:large_green_square:
-🟪	:large_purple_square:
-🟫	:large_brown_square:
-🤍	:white_heart:
-🤎	:brown_heart:
-🤏	:pinching_hand:
-🤿	:diving_mask:
-🥱	:yawning_face:
-🥻	:sari:
-🦥	:sloth:
-🦦	:otter:
-🦧	:orangutan:
-🦨	:skunk:
-🦩	:flamingo:
-🦪	:oyster:
-🦮	:guide_dog:
-🦯	:probing_cane:
-🦺	:safety_vest:
-🦻	:ear_with_hearing_aid:
-🦼	:motorized_wheelchair:
-🦽	:manual_wheelchair:
-🦾	:mechanical_arm:
-🦿	:mechanical_leg:
-🧃	:beverage_box:
-🧄	:garlic:
-🧅	:onion:
-🧆	:falafel:
-🧇	:waffle:
-🧈	:butter:
-🧉	:mate_drink:
-🧊	:ice_cube:
-🧍‍♀️	:woman_standing:
-🧍‍♂️	:man_standing:
-🧍	:standing_person:
-🧎‍♀️	:woman_kneeling:
-🧎‍♂️	:man_kneeling:
-🧎	:kneeling_person:
-🧏‍♀️	:deaf_woman:
-🧏‍♂️	:deaf_man:
-🧏	:deaf_person:
-🧑‍🦯	:person_with_probing_cane:
-🧑‍🦼	:person_in_motorized_wheelchair:
-🧑‍🦽	:person_in_manual_wheelchair:
-🩰	:ballet_shoes:
-🩱	:one-piece_swimsuit:
-🩲	:briefs:
-🩳	:shorts:
-🩸	:drop_of_blood:
-🩹	:adhesive_bandage:
-🩺	:stethoscope:
-🪀	:yo-yo:
-🪁	:kite:
-🪂	:parachute:
-🪐	:ringed_planet:
-🪑	:chair:
-🪒	:razor:
-🪓	:axe:
-🪔	:diya_lamp:
-🪕	:banjo:
-    '''),
-  NewsItem(
-    // https://unicodey.com/emoji-data/table.htm
-    content:
-        '''#️⃣ *️⃣ 0️⃣ 1️⃣ 2️⃣ 3️⃣ 4️⃣ 5️⃣ 6️⃣ 7️⃣ 8️⃣ 9️⃣ ©️ ®️ 🀄 🃏 🅰️ 🅱️ 🅾️ 🅿️ 🆎 🆑 🆒 🆓 🆔 🆕 🆖 🆗 🆘 🆙 🆚 🇦🇨 🇦🇩 🇦🇪 🇦🇫 🇦🇬 🇦🇮 🇦🇱 🇦🇲 🇦🇴 🇦🇶 🇦🇷 🇦🇸 🇦🇹 🇦🇺 🇦🇼 🇦🇽 🇦🇿 🇧🇦 🇧🇧 🇧🇩 🇧🇪 🇧🇫 🇧🇬 🇧🇭 🇧🇮 🇧🇯 🇧🇱 🇧🇲 🇧🇳 🇧🇴 🇧🇶 🇧🇷 🇧🇸 🇧🇹 🇧🇻 🇧🇼 🇧🇾 🇧🇿 🇨🇦 🇨🇨 🇨🇩 🇨🇫 🇨🇬 🇨🇭 🇨🇮 🇨🇰 🇨🇱 🇨🇲 🇨🇳 🇨🇴 🇨🇵 🇨🇷 🇨🇺 🇨🇻 🇨🇼 🇨🇽 🇨🇾 🇨🇿 🇩🇪 🇩🇬 🇩🇯 🇩🇰 🇩🇲 🇩🇴 🇩🇿 🇪🇦 🇪🇨 🇪🇪 🇪🇬 🇪🇭 🇪🇷 🇪🇸 🇪🇹 🇪🇺 🇫🇮 🇫🇯 🇫🇰 🇫🇲 🇫🇴 🇫🇷 🇬🇦 🇬🇧 🇬🇩 🇬🇪 🇬🇫 🇬🇬 🇬🇭 🇬🇮 🇬🇱 🇬🇲 🇬🇳 🇬🇵 🇬🇶 🇬🇷 🇬🇸 🇬🇹 🇬🇺 🇬🇼 🇬🇾 🇭🇰 🇭🇲 🇭🇳 🇭🇷 🇭🇹 🇭🇺 🇮🇨 🇮🇩 🇮🇪 🇮🇱 🇮🇲 🇮🇳 🇮🇴 🇮🇶 🇮🇷 🇮🇸 🇮🇹 🇯🇪 🇯🇲 🇯🇴 🇯🇵 🇰🇪 🇰🇬 🇰🇭 🇰🇮 🇰🇲 🇰🇳 🇰🇵 🇰🇷 🇰🇼 🇰🇾 🇰🇿 🇱🇦 🇱🇧 🇱🇨 🇱🇮 🇱🇰 🇱🇷 🇱🇸 🇱🇹 🇱🇺 🇱🇻 🇱🇾 🇲🇦 🇲🇨 🇲🇩 🇲🇪 🇲🇫 🇲🇬 🇲🇭 🇲🇰 🇲🇱 🇲🇲 🇲🇳 🇲🇴 🇲🇵 🇲🇶 🇲🇷 🇲🇸 🇲🇹 🇲🇺 🇲🇻 🇲🇼 🇲🇽 🇲🇾 🇲🇿 🇳🇦 🇳🇨 🇳🇪 🇳🇫 🇳🇬 🇳🇮 🇳🇱 🇳🇴 🇳🇵 🇳🇷 🇳🇺 🇳🇿 🇴🇲 🇵🇦 🇵🇪 🇵🇫 🇵🇬 🇵🇭 🇵🇰 🇵🇱 🇵🇲 🇵🇳 🇵🇷 🇵🇸 🇵🇹 🇵🇼 🇵🇾 🇶🇦 🇷🇪 🇷🇴 🇷🇸 🇷🇺 🇷🇼 🇸🇦 🇸🇧 🇸🇨 🇸🇩 🇸🇪 🇸🇬 🇸🇭 🇸🇮 🇸🇯 🇸🇰 🇸🇱 🇸🇲 🇸🇳 🇸🇴 🇸🇷 🇸🇸 🇸🇹 🇸🇻 🇸🇽 🇸🇾 🇸🇿 🇹🇦 🇹🇨 🇹🇩 🇹🇫 🇹🇬 🇹🇭 🇹🇯 🇹🇰 🇹🇱 🇹🇲 🇹🇳 🇹🇴 🇹🇷 🇹🇹 🇹🇻 🇹🇼 🇹🇿 🇺🇦 🇺🇬 🇺🇲 🇺🇳 🇺🇸 🇺🇾 🇺🇿 🇻🇦 🇻🇨 🇻🇪 🇻🇬 🇻🇮 🇻🇳 🇻🇺 🇼🇫 🇼🇸 🇽🇰 🇾🇪 🇾🇹 🇿🇦 🇿🇲 🇿🇼 🈁 🈂️ 🈚 🈯 🈲 🈳 🈴 🈵 🈶 🈷️ 🈸 🈹 🈺 🉐 🉑 🌀 🌁 🌂 🌃 🌄 🌅 🌆 🌇 🌈 🌉 🌊 🌋 🌌 🌍 🌎 🌏 🌐 🌑 🌒 🌓 🌔 🌕 🌖 🌗 🌘 🌙 🌚 🌛 🌜 🌝 🌞 🌟 🌠 🌡️ 🌤️ 🌥️ 🌦️ 🌧️ 🌨️ 🌩️ 🌪️ 🌫️ 🌬️ 🌭 🌮 🌯 🌰 🌱 🌲 🌳 🌴 🌵 🌶️ 🌷 🌸 🌹 🌺 🌻 🌼 🌽 🌾 🌿 🍀 🍁 🍂 🍃 🍄 🍅 🍆 🍇 🍈 🍉 🍊 🍋 🍌 🍍 🍎 🍏 🍐 🍑 🍒 🍓 🍔 🍕 🍖 🍗 🍘 🍙 🍚 🍛 🍜 🍝 🍞 🍟 🍠 🍡 🍢 🍣 🍤 🍥 🍦 🍧 🍨 🍩 🍪 🍫 🍬 🍭 🍮 🍯 🍰 🍱 🍲 🍳 🍴 🍵 🍶 🍷 🍸 🍹 🍺 🍻 🍼 🍽️ 🍾 🍿 🎀 🎁 🎂 🎃 🎄 🎅 🎆 🎇 🎈 🎉 🎊 🎋 🎌 🎍 🎎 🎏 🎐 🎑 🎒 🎓 🎖️ 🎗️ 🎙️ 🎚️ 🎛️ 🎞️ 🎟️ 🎠 🎡 🎢 🎣 🎤 🎥 🎦 🎧 🎨 🎩 🎪 🎫 🎬 🎭 🎮 🎯 🎰 🎱 🎲 🎳 🎴 🎵 🎶 🎷 🎸 🎹 🎺 🎻 🎼 🎽 🎾 🎿 🏀 🏁 🏂 🏃‍♀️ 🏃‍♂️ 🏃 🏄‍♀️ 🏄‍♂️ 🏄 🏅 🏆 🏇 🏈 🏉 🏊‍♀️ 🏊‍♂️ 🏊 🏋️‍♀️ 🏋️‍♂️ 🏋️ 🏌️‍♀️ 🏌️‍♂️ 🏌️ 🏍️ 🏎️ 🏏 🏐 🏑 🏒 🏓 🏔️ 🏕️ 🏖️ 🏗️ 🏘️ 🏙️ 🏚️ 🏛️ 🏜️ 🏝️ 🏞️ 🏟️ 🏠 🏡 🏢 🏣 🏤 🏥 🏦 🏧 🏨 🏩 🏪 🏫 🏬 🏭 🏮 🏯 🏰 🏳️‍🌈 🏳️ 🏴‍☠️ 🏴󠁧󠁢󠁥󠁮󠁧󠁿 🏴󠁧󠁢󠁳󠁣󠁴󠁿 🏴󠁧󠁢󠁷󠁬󠁳󠁿 🏴 🏵️ 🏷️ 🏸 🏹 🏺 🏻 🏼 🏽 🏾 🏿 🐀 🐁 🐂 🐃 🐄 🐅 🐆 🐇 🐈 🐉 🐊 🐋 🐌 🐍 🐎 🐏 🐐 🐑 🐒 🐓 🐔 🐕‍🦺 🐕 🐖 🐗 🐘 🐙 🐚 🐛 🐜 🐝 🐞 🐟 🐠 🐡 🐢 🐣 🐤 🐥 🐦 🐧 🐨 🐩 🐪 🐫 🐬 🐭 🐮 🐯 🐰 🐱 🐲 🐳 🐴 🐵 🐶 🐷 🐸 🐹 🐺 🐻 🐼 🐽 🐾 🐿️ 👀 👁️‍🗨️ 👁️ 👂 👃 👄 👅 👆 👇 👈 👉 👊 👋 👌 👍 👎 👏 👐 👑 👒 👓 👔 👕 👖 👗 👘 👙 👚 👛 👜 👝 👞 👟 👠 👡 👢 👣 👤 👥 👦 👧 👨‍🌾 👨‍🍳 👨‍🎓 👨‍🎤 👨‍🎨 👨‍🏫 👨‍🏭 👨‍👦‍👦 👨‍👦 👨‍👧‍👦 👨‍👧‍👧 👨‍👧 👨‍👨‍👦 👨‍👨‍👦‍👦 👨‍👨‍👧 👨‍👨‍👧‍👦 👨‍👨‍👧‍👧 👨‍👩‍👦 👨‍👩‍👦‍👦 👨‍👩‍👧 👨‍👩‍👧‍👦 👨‍👩‍👧‍👧 👨‍💻 👨‍💼 👨‍🔧 👨‍🔬 👨‍🚀 👨‍🚒 👨‍🦯 👨‍🦰 👨‍🦱 👨‍🦲 👨‍🦳 👨‍🦼 👨‍🦽 👨‍⚕️ 👨‍⚖️ 👨‍✈️ 👨‍❤️‍👨 👨‍❤️‍💋‍👨 👨 👩‍🌾 👩‍🍳 👩‍🎓 👩‍🎤 👩‍🎨 👩‍🏫 👩‍🏭 👩‍👦‍👦 👩‍👦 👩‍👧‍👦 👩‍👧‍👧 👩‍👧 👩‍👩‍👦 👩‍👩‍👦‍👦 👩‍👩‍👧 👩‍👩‍👧‍👦 👩‍👩‍👧‍👧 👩‍💻 👩‍💼 👩‍🔧 👩‍🔬 👩‍🚀 👩‍🚒 👩‍🦯 👩‍🦰 👩‍🦱 👩‍🦲 👩‍🦳 👩‍🦼 👩‍🦽 👩‍⚕️ 👩‍⚖️ 👩‍✈️ 👩‍❤️‍👨 👩‍❤️‍👩 👩‍❤️‍💋‍👨 👩‍❤️‍💋‍👩 👩 👪 👫 👬 👭 👮‍♀️ 👮‍♂️ 👮 👯‍♀️ 👯‍♂️ 👯 👰 👱‍♀️ 👱‍♂️ 👱 👲 👳‍♀️ 👳‍♂️ 👳 👴 👵 👶 👷‍♀️ 👷‍♂️ 👷 👸 👹 👺 👻 👼 👽 👾 👿 💀 💁‍♀️ 💁‍♂️ 💁 💂‍♀️ 💂‍♂️ 💂 💃 💄 💅 💆‍♀️ 💆‍♂️ 💆 💇‍♀️ 💇‍♂️ 💇 💈 💉 💊 💋 💌 💍 💎 💏 💐 💑 💒 💓 💔 💕 💖 💗 💘 💙 💚 💛 💜 💝 💞 💟 💠 💡 💢 💣 💤 💥 💦 💧 💨 💩 💪 💫 💬 💭 💮 💯 💰 💱 💲 💳 💴 💵 💶 💷 💸 💹 💺 💻 💼 💽 💾 💿 📀 📁 📂 📃 📄 📅 📆 📇 📈 📉 📊 📋 📌 📍 📎 📏 📐 📑 📒 📓 📔 📕 📖 📗 📘 📙 📚 📛 📜 📝 📞 📟 📠 📡 📢 📣 📤 📥 📦 📧 📨 📩 📪 📫 📬 📭 📮 📯 📰 📱 📲 📳 📴 📵 📶 📷 📸 📹 📺 📻 📼 📽️ 📿 🔀 🔁 🔂 🔃 🔄 🔅 🔆 🔇 🔈 🔉 🔊 🔋 🔌 🔍 🔎 🔏 🔐 🔑 🔒 🔓 🔔 🔕 🔖 🔗 🔘 🔙 🔚 🔛 🔜 🔝 🔞 🔟 🔠 🔡 🔢 🔣 🔤 🔥 🔦 🔧 🔨 🔩 🔪 🔫 🔬 🔭 🔮 🔯 🔰 🔱 🔲 🔳 🔴 🔵 🔶 🔷 🔸 🔹 🔺 🔻 🔼 🔽 🕉️ 🕊️ 🕋 🕌 🕍 🕎 🕐 🕑 🕒 🕓 🕔 🕕 🕖 🕗 🕘 🕙 🕚 🕛 🕜 🕝 🕞 🕟 🕠 🕡 🕢 🕣 🕤 🕥 🕦 🕧 🕯️ 🕰️ 🕳️ 🕴️ 🕵️‍♀️ 🕵️‍♂️ 🕵️ 🕶️ 🕷️ 🕸️ 🕹️ 🕺 🖇️ 🖊️ 🖋️ 🖌️ 🖍️ 🖐️ 🖕 🖖 🖤 🖥️ 🖨️ 🖱️ 🖲️ 🖼️ 🗂️ 🗃️ 🗄️ 🗑️ 🗒️ 🗓️ 🗜️ 🗝️ 🗞️ 🗡️ 🗣️ 🗨️ 🗯️ 🗳️ 🗺️ 🗻 🗼 🗽 🗾 🗿 😀 😁 😂 😃 😄 😅 😆 😇 😈 😉 😊 😋 😌 😍 😎 😏 😐 😑 😒 😓 😔 😕 😖 😗 😘 😙 😚 😛 😜 😝 😞 😟 😠 😡 😢 😣 😤 😥 😦 😧 😨 😩 😪 😫 😬 😭 😮 😯 😰 😱 😲 😳 😴 😵 😶 😷 😸 😹 😺 😻 😼 😽 😾 😿 🙀 🙁 🙂 🙃 🙄 🙅‍♀️ 🙅‍♂️ 🙅 🙆‍♀️ 🙆‍♂️ 🙆 🙇‍♀️ 🙇‍♂️ 🙇 🙈 🙉 🙊 🙋‍♀️ 🙋‍♂️ 🙋 🙌 🙍‍♀️ 🙍‍♂️ 🙍 🙎‍♀️ 🙎‍♂️ 🙎 🙏 🚀 🚁 🚂 🚃 🚄 🚅 🚆 🚇 🚈 🚉 🚊 🚋 🚌 🚍 🚎 🚏 🚐 🚑 🚒 🚓 🚔 🚕 🚖 🚗 🚘 🚙 🚚 🚛 🚜 🚝 🚞 🚟 🚠 🚡 🚢 🚣‍♀️ 🚣‍♂️ 🚣 🚤 🚥 🚦 🚧 🚨 🚩 🚪 🚫 🚬 🚭 🚮 🚯 🚰 🚱 🚲 🚳 🚴‍♀️ 🚴‍♂️ 🚴 🚵‍♀️ 🚵‍♂️ 🚵 🚶‍♀️ 🚶‍♂️ 🚶 🚷 🚸 🚹 🚺 🚻 🚼 🚽 🚾 🚿 🛀 🛁 🛂 🛃 🛄 🛅 🛋️ 🛌 🛍️ 🛎️ 🛏️ 🛐 🛑 🛒 🛕 🛠️ 🛡️ 🛢️ 🛣️ 🛤️ 🛥️ 🛩️ 🛫 🛬 🛰️ 🛳️ 🛴 🛵 🛶 🛷 🛸 🛹 🛺 🟠 🟡 🟢 🟣 🟤 🟥 🟦 🟧 🟨 🟩 🟪 🟫 🤍 🤎 🤏 🤐 🤑 🤒 🤓 🤔 🤕 🤖 🤗 🤘 🤙 🤚 🤛 🤜 🤝 🤞 🤟 🤠 🤡 🤢 🤣 🤤 🤥 🤦‍♀️ 🤦‍♂️ 🤦 🤧 🤨 🤩 🤪 🤫 🤬 🤭 🤮 🤯 🤰 🤱 🤲 🤳 🤴 🤵 🤶 🤷‍♀️ 🤷‍♂️ 🤷 🤸‍♀️ 🤸‍♂️ 🤸 🤹‍♀️ 🤹‍♂️ 🤹 🤺 🤼‍♀️ 🤼‍♂️ 🤼 🤽‍♀️ 🤽‍♂️ 🤽 🤾‍♀️ 🤾‍♂️ 🤾 🤿 🥀 🥁 🥂 🥃 🥄 🥅 🥇 🥈 🥉 🥊 🥋 🥌 🥍 🥎 🥏 🥐 🥑 🥒 🥓 🥔 🥕 🥖 🥗 🥘 🥙 🥚 🥛 🥜 🥝 🥞 🥟 🥠 🥡 🥢 🥣 🥤 🥥 🥦 🥧 🥨 🥩 🥪 🥫 🥬 🥭 🥮 🥯 🥰 🥱 🥳 🥴 🥵 🥶 🥺 🥻 🥼 🥽 🥾 🥿 🦀 🦁 🦂 🦃 🦄 🦅 🦆 🦇 🦈 🦉 🦊 🦋 🦌 🦍 🦎 🦏 🦐 🦑 🦒 🦓 🦔 🦕 🦖 🦗 🦘 🦙 🦚 🦛 🦜 🦝 🦞 🦟 🦠 🦡 🦢 🦥 🦦 🦧 🦨 🦩 🦪 🦮 🦯 🦴 🦵 🦶 🦷 🦸‍♀️ 🦸‍♂️ 🦸 🦹‍♀️ 🦹‍♂️ 🦹 🦺 🦻 🦼 🦽 🦾 🦿 🧀 🧁 🧂 🧃 🧄 🧅 🧆 🧇 🧈 🧉 🧊 🧍‍♀️ 🧍‍♂️ 🧍 🧎‍♀️ 🧎‍♂️ 🧎 🧏‍♀️ 🧏‍♂️ 🧏 🧐 🧑‍🌾 🧑‍🍳 🧑‍🎓 🧑‍🎤 🧑‍🎨 🧑‍🏫 🧑‍🏭 🧑‍💻 🧑‍💼 🧑‍🔧 🧑‍🔬 🧑‍🚀 🧑‍🚒 🧑‍🤝‍🧑 🧑‍🦯 🧑‍🦰 🧑‍🦱 🧑‍🦲 🧑‍🦳 🧑‍🦼 🧑‍🦽 🧑‍⚕️ 🧑‍⚖️ 🧑‍✈️ 🧑 🧒 🧓 🧔 🧕 🧖‍♀️ 🧖‍♂️ 🧖 🧗‍♀️ 🧗‍♂️ 🧗 🧘‍♀️ 🧘‍♂️ 🧘 🧙‍♀️ 🧙‍♂️ 🧙 🧚‍♀️ 🧚‍♂️ 🧚 🧛‍♀️ 🧛‍♂️ 🧛 🧜‍♀️ 🧜‍♂️ 🧜 🧝‍♀️ 🧝‍♂️ 🧝 🧞‍♀️ 🧞‍♂️ 🧞 🧟‍♀️ 🧟‍♂️ 🧟 🧠 🧡 🧢 🧣 🧤 🧥 🧦 🧧 🧨 🧩 🧪 🧫 🧬 🧭 🧮 🧯 🧰 🧱 🧲 🧳 🧴 🧵 🧶 🧷 🧸 🧹 🧺 🧻 🧼 🧽 🧾 🧿 🩰 🩱 🩲 🩳 🩸 🩹 🩺 🪀 🪁 🪂 🪐 🪑 🪒 🪓 🪔 🪕 ‼️ ⁉️ ™️ ℹ️ ↔️ ↕️ ↖️ ↗️ ↘️ ↙️ ↩️ ↪️ ⌚ ⌛ ⌨️ ⏏️ ⏩ ⏪ ⏫ ⏬ ⏭️ ⏮️ ⏯️ ⏰ ⏱️ ⏲️ ⏳ ⏸️ ⏹️ ⏺️ Ⓜ️ ▪️ ▫️ ▶️ ◀️ ◻️ ◼️ ◽ ◾ ☀️ ☁️ ☂️ ☃️ ☄️ ☎️ ☑️ ☔ ☕ ☘️ ☝️ ☠️ ☢️ ☣️ ☦️ ☪️ ☮️ ☯️ ☸️ ☹️ ☺️ ♀️ ♂️ ♈ ♉ ♊ ♋ ♌ ♍ ♎ ♏ ♐ ♑ ♒ ♓ ♟️ ♠️ ♣️ ♥️ ♦️ ♨️ ♻️ ♾️ ♿ ⚒️ ⚓ ⚔️ ⚕️ ⚖️ ⚗️ ⚙️ ⚛️ ⚜️ ⚠️ ⚡ ⚪ ⚫ ⚰️ ⚱️ ⚽ ⚾ ⛄ ⛅ ⛈️ ⛎ ⛏️ ⛑️ ⛓️ ⛔ ⛩️ ⛪ ⛰️ ⛱️ ⛲ ⛳ ⛴️ ⛵ ⛷️ ⛸️ ⛹️‍♀️ ⛹️‍♂️ ⛹️ ⛺ ⛽ ✂️ ✅ ✈️ ✉️ ✊ ✋ ✌️ ✍️ ✏️ ✒️ ✔️ ✖️ ✝️ ✡️ ✨ ✳️ ✴️ ❄️ ❇️ ❌ ❎ ❓ ❔ ❕ ❗ ❣️ ❤️ ➕ ➖ ➗ ➡️ ➰ ➿ ⤴️ ⤵️ ⬅️ ⬆️ ⬇️ ⬛ ⬜ ⭐ ⭕ 〰️ 〽️ ㊗️ ㊙️''',
-  ),
-];
+String newsPlaceholder =
+    r'''[{"date": "2020-06-18 07:51:09.541000", "content": "@everyone \n\nKomodo will be on Turkish television!\n\nhttps://twitter.com/KomodoPlatform/status/1271813884098289671?s=20"}, {"date": "2020-06-18 07:18:48.906000", "content": "@everyone \n\nWe have published another article from our Blockchain Fundamentals series. 📚\n\nBlockchain Programming Languages: An Introductory Guide\n\nIf you’re a developer looking to learn the right languages to begin your blockchain career, then this post is for you. In this article, we cover the most in-demand blockchain programming languages. We’ll look at how each language is used in the context of blockchain programming, along with examples from major projects in the blockchain space. The three categories featured are protocol-level development, smart contract development, and software development kits (SDKs).\n\nRead more here.\nhttps://komodoplatform.com/blockchain-programming-languages/"}, {"date": "2020-05-02 18:54:48.795000", "content": ""}, {"date": "2020-06-18 07:51:09.541000", "content": "@everyone \n\nKomodo will be on Turkish television!\n\nhttps://twitter.com/KomodoPlatform/status/1271813884098289671?s=20"}, {"date": "2020-06-18 07:18:48.906000", "content": "@everyone \n\nWe have published another article from our Blockchain Fundamentals series. 📚\n\nBlockchain Programming Languages: An Introductory Guide\n\nIf you’re a developer looking to learn the right languages to begin your blockchain career, then this post is for you. In this article, we cover the most in-demand blockchain programming languages. We’ll look at how each language is used in the context of blockchain programming, along with examples from major projects in the blockchain space. The three categories featured are protocol-level development, smart contract development, and software development kits (SDKs).\n\nRead more here.\nhttps://komodoplatform.com/blockchain-programming-languages/"}]''';
