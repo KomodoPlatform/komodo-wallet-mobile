1510380c8f3f8cba86b7aebc73572fc2c8df2399
yurii <yuriikhiznichenko@gmail.com>
2020-10-30 12:59:17 +0100

    WIP: fix iOs audio interruption by system sounds

--- ios/Runner/audio.m
+++ ios/Runner/audio.m
@@ -117,7 +117,7 @@ void audio_init (const char* assets_maker) {
 
   //os_log (OS_LOG_DEFAULT, "audio_init] Category..");
   err = nil;
-  [[AVAudioSession sharedInstance] setCategory: AVAudioSessionCategoryPlayback error: &err];
+  [[AVAudioSession sharedInstance] setCategory: AVAudioSessionCategoryPlayback withOptions: AVAudioSessionCategoryOptionMixWithOthers error: &err];
   if (err) {os_log (OS_LOG_DEFAULT, "audio_init] !setCategory: %{public}@", err); return;}
 
   //os_log (OS_LOG_DEFAULT, "audio_init] Starting..");
