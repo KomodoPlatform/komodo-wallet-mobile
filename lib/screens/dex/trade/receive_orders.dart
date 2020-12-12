6864b5eceac4dec61d3285ec983e513fd74dd36a
yurii <khiznichenko@gmail.com>
2020-12-12 11:27:16 +0100

    Limit visible orders list lenght in DEX section

--- lib/screens/dex/trade/receive_orders.dart
+++ lib/screens/dex/trade/receive_orders.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:komodo_dex/blocs/coins_bloc.dart';
 import 'package:komodo_dex/blocs/dialog_bloc.dart';
@@ -189,6 +191,10 @@ class _AsksOrderState extends State<AsksOrder> {
   final double headerHeight = 50;
   final double lineHeight = 50;
   bool popupSettingsVisible = false;
+  int listLength = 0;
+  int listLimit = 25;
+  int listLimitMin = 25;
+  int listLimitStep = 5;
   OrderBookProvider orderBookProvider;
   CexProvider cexProvider;
   AddressBookProvider addressBookProvider;
@@ -206,6 +212,10 @@ class _AsksOrderState extends State<AsksOrder> {
     List<Ask> bidsList = orderbook?.bids;
 
     bidsList = OrderBookProvider.sortByPrice(bidsList, quotePrice: true);
+    setState(() {
+      listLength = bidsList.length;
+    });
+    if (bidsList.length > listLimit) bidsList = bidsList.sublist(0, listLimit);
     bidsList?.asMap()?.forEach(
         (int index, Ask bid) => asksWidget.add(_tableRow(bid, index)));
 
@@ -348,6 +358,7 @@ class _AsksOrderState extends State<AsksOrder> {
                                   ],
                                 ),
                               ),
+                        _buildLimitButton(),
                         CreateOrder(
                           onCreateNoOrder: widget.onCreateNoOrder,
                           coin: relCoin,
@@ -357,6 +368,57 @@ class _AsksOrderState extends State<AsksOrder> {
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildLimitButton() {
+    if (listLength < listLimit && listLimit == listLimitMin) return SizedBox();
+
+    return Container(
+      padding: EdgeInsets.all(12),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Text(
+            'Showing $listLimit of ${max(listLength, listLimit)} orders. ',
+            style: Theme.of(context).textTheme.bodyText1,
+          ),
+          if (listLimit > listLimitMin)
+            InkWell(
+                onTap: () {
+                  setState(() {
+                    listLimit -= listLimitStep;
+                  });
+                },
+                child: Container(
+                  padding: EdgeInsets.all(6),
+                  child: Text(
+                    'Less',
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Theme.of(context).accentColor,
+                    ),
+                  ),
+                )),
+          if (listLength > listLimit)
+            InkWell(
+                onTap: () {
+                  setState(() {
+                    listLimit += listLimitStep;
+                  });
+                },
+                child: Container(
+                  padding: EdgeInsets.all(6),
+                  child: Text(
+                    'More',
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Theme.of(context).accentColor,
+                    ),
+                  ),
+                )),
+        ],
       ),
     );
   }
