46b4bbb210974046bf055da0c459bad17ebe2818
yurii <khiznichenko@gmail.com>
2021-01-14 13:34:40 +0100

    Auto switch swap page with maker-order page
    If taker order was transformed to maker after 30 sec.

--- lib/screens/dex/history/swap_detail_page/swap_detail_page.dart
+++ lib/screens/dex/history/swap_detail_page/swap_detail_page.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:komodo_dex/blocs/orders_bloc.dart';
 import 'package:komodo_dex/blocs/swap_history_bloc.dart';
+import 'package:komodo_dex/model/order.dart';
 import 'package:komodo_dex/model/swap.dart';
 import 'package:komodo_dex/model/swap_provider.dart';
 import 'package:komodo_dex/screens/authentification/lock_screen.dart';
 import 'package:komodo_dex/screens/dex/history/swap_detail_page/final_trade_success.dart';
 import 'package:komodo_dex/screens/dex/history/swap_detail_page/stepper_trade.dart';
+import 'package:komodo_dex/screens/dex/orders/maker_order_details_page.dart';
 import 'package:komodo_dex/widgets/sound_volume_button.dart';
 
 class SwapDetailPage extends StatefulWidget {
@@ -29,57 +32,90 @@ class _SwapDetailPageState extends State<SwapDetailPage> {
 
   @override
   Widget build(BuildContext context) {
-    return LockScreen(
-      context: context,
-      onSuccess: () {},
-      child: Scaffold(
-        backgroundColor: Theme.of(context).backgroundColor,
-        appBar: AppBar(
-            elevation: 0,
-            backgroundColor: Theme.of(context).backgroundColor,
-            leading: IconButton(
-              key: const Key('swap-detail-back-button'),
-              icon: Icon(Icons.arrow_back),
-              onPressed: () => Navigator.pop(context, false),
+    return StreamBuilder<List<dynamic>>(
+        initialData: ordersBloc.orderSwaps,
+        stream: ordersBloc.outOrderSwaps,
+        builder: (BuildContext context,
+            AsyncSnapshot<List<dynamic>> ordersSnapshot) {
+          _switchToMakerIfTransformed(ordersSnapshot);
+
+          return LockScreen(
+            context: context,
+            onSuccess: () {},
+            child: Scaffold(
+              backgroundColor: Theme.of(context).backgroundColor,
+              appBar: AppBar(
+                  elevation: 0,
+                  backgroundColor: Theme.of(context).backgroundColor,
+                  leading: IconButton(
+                    key: const Key('swap-detail-back-button'),
+                    icon: Icon(Icons.arrow_back),
+                    onPressed: () => Navigator.pop(context, false),
+                  ),
+                  actions: const <Widget>[
+                    SoundVolumeButton(key: Key('swap-detail-sound-button'))
+                  ]),
+              body: StreamBuilder<Iterable<Swap>>(
+                  stream: swapHistoryBloc.outSwaps,
+                  initialData: swapMonitor.swaps,
+                  builder: (BuildContext context,
+                      AsyncSnapshot<Iterable<Swap>> swapsSnapshot) {
+                    if (swapsSnapshot.data != null &&
+                        swapsSnapshot.data.isNotEmpty) {
+                      swapData = widget.swap;
+                      for (Swap swap in swapsSnapshot.data) {
+                        if (swap.result.uuid == widget.swap.result.uuid) {
+                          swapData = swap;
+                        }
+                      }
+                      if (swapData.status == Status.SWAP_SUCCESSFUL &&
+                          swapHistoryBloc.isAnimationStepFinalIsFinish) {
+                        return FinalTradeSuccess(swap: swapData);
+                      } else {
+                        return StepperTrade(
+                            swap: swapData,
+                            onStepFinish: () {
+                              setState(() {
+                                swapHistoryBloc.isAnimationStepFinalIsFinish =
+                                    true;
+                              });
+                            });
+                      }
+                    } else {
+                      return StepperTrade(
+                          swap: widget.swap,
+                          onStepFinish: () {
+                            setState(() {
+                              swapHistoryBloc.isAnimationStepFinalIsFinish =
+                                  true;
+                            });
+                          });
+                    }
+                  }),
             ),
-            actions: const <Widget>[
-              SoundVolumeButton(key: Key('swap-detail-sound-button'))
-            ]),
-        body: StreamBuilder<Iterable<Swap>>(
-            stream: swapHistoryBloc.outSwaps,
-            initialData: swapMonitor.swaps,
-            builder:
-                (BuildContext context, AsyncSnapshot<Iterable<Swap>> snapshot) {
-              if (snapshot.data != null && snapshot.data.isNotEmpty) {
-                swapData = widget.swap;
-                for (Swap swap in snapshot.data) {
-                  if (swap.result.uuid == widget.swap.result.uuid) {
-                    swapData = swap;
-                  }
-                }
-                if (swapData.status == Status.SWAP_SUCCESSFUL &&
-                    swapHistoryBloc.isAnimationStepFinalIsFinish) {
-                  return FinalTradeSuccess(swap: swapData);
-                } else {
-                  return StepperTrade(
-                      swap: swapData,
-                      onStepFinish: () {
-                        setState(() {
-                          swapHistoryBloc.isAnimationStepFinalIsFinish = true;
-                        });
-                      });
-                }
-              } else {
-                return StepperTrade(
-                    swap: widget.swap,
-                    onStepFinish: () {
-                      setState(() {
-                        swapHistoryBloc.isAnimationStepFinalIsFinish = true;
-                      });
-                    });
-              }
-            }),
-      ),
-    );
+          );
+        });
+  }
+
+  void _switchToMakerIfTransformed(
+      AsyncSnapshot<List<dynamic>> ordersSnapshot) {
+    if (!ordersSnapshot.hasData) return;
+
+    final Order makerOrder = ordersBloc.orderSwaps.firstWhere((dynamic order) {
+      return order is Order &&
+          order.uuid == widget.swap.result.uuid &&
+          order.orderType == OrderType.MAKER;
+    }, orElse: () => null);
+
+    if (makerOrder == null) return;
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Navigator.pushReplacement<dynamic, dynamic>(
+        context,
+        MaterialPageRoute<dynamic>(
+            builder: (BuildContext context) =>
+                MakerOrderDetailsPage(makerOrder.uuid)),
+      );
+    });
   }
 }
