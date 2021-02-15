ad7e429a9f57017b4aa111b4c9780564e1b72b1f
Mateus Rodrigues Costa <mateusrodcosta@gmail.com>
2021-02-15 17:41:17 -0300

    Improve sell coin selection onPressed

--- lib/screens/dex/trade/trade_page.dart
+++ lib/screens/dex/trade/trade_page.dart
@@ -961,12 +961,9 @@ class _TradePageState extends State<TradePage> with TickerProviderStateMixin {
               swapBloc.updateReceiveCoin(null);
               swapBloc.setTimeout(true);
               _controllerAmountReceive.clear();
+              _controllerAmountSell.clear();
               setState(() {
                 sellCoinBalance = coin;
-                final String tmp = _controllerAmountSell.text;
-                _controllerAmountSell.text = '';
-                _controllerAmountSell.text = tmp;
-                _controllerAmountReceive.text = '';
                 swapBloc.setEnabledSellField(true);
               });
               swapBloc.updateSellCoin(coin);
@@ -975,7 +972,6 @@ class _TradePageState extends State<TradePage> with TickerProviderStateMixin {
                 buy: orderBookProvider.activePair?.buy,
               );
               swapBloc.updateBuyCoin(null);
-              _controllerAmountSell.clear();
 
               Navigator.pop(context);
             },
