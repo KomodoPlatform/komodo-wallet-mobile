f040016bdc662911fc35435d7e1fb4a853387405
Mateus Rodrigues Costa <mateusrodcosta@gmail.com>
2021-03-31 13:49:25 -0300

    Implement scrolling for long ticker names
    Related to #1123

--- lib/screens/dex/trade/trade_page.dart
+++ lib/screens/dex/trade/trade_page.dart
@@ -697,12 +697,20 @@ class _TradePageState extends State<TradePage> with TickerProviderStateMixin {
                       radius: 12,
                     ),
               Expanded(
-                  child: Center(
-                      child: Text(
-                coin != null ? coin.abbr : '-',
-                style: Theme.of(context).textTheme.subtitle2,
-                maxLines: 1,
-              ))),
+                child: Center(
+                  child: coin != null
+                      ? CoinScrollText(
+                          abbr: coin.abbr,
+                          style: Theme.of(context).textTheme.subtitle2,
+                          maxLines: 1,
+                        )
+                      : Text(
+                          '-',
+                          style: Theme.of(context).textTheme.subtitle2,
+                          maxLines: 1,
+                        ),
+                ),
+              ),
               Icon(Icons.arrow_drop_down),
             ],
           ),
@@ -1253,5 +1261,89 @@ class _DialogLookingState extends State<DialogLooking> {
         ),
       ),
     );
+  }
+}
+
+class CoinScrollText extends StatefulWidget {
+  const CoinScrollText({Key key, this.abbr, this.style, this.maxLines})
+      : super(key: key);
+  final String abbr;
+  final TextStyle style;
+  final int maxLines;
+
+  @override
+  _CoinScrollTextState createState() => _CoinScrollTextState();
+}
+
+class _CoinScrollTextState extends State<CoinScrollText> {
+  Timer _scrollTimer;
+  int _scrollAbbrStart = 0;
+  int _scrollAbbrEnd = 0;
+  int _scrollAbbrLength = 0;
+  int _spacesBefore = 0;
+  int _spacesAfter = 0;
+  bool _isScrollStart = true;
+  String text = '-';
+
+  String coinScroll(String abbr, {int maxChar}) {
+    maxChar ??= 5;
+    if (abbr.length <= maxChar) return abbr;
+    if (_isScrollStart) {
+      _scrollAbbrStart = 0;
+      _scrollAbbrEnd = 1;
+      _scrollAbbrLength = abbr.length;
+      _spacesBefore = maxChar - 1;
+      _spacesAfter = 0;
+      _isScrollStart = false;
+    } else {
+      if (_spacesBefore > 0) {
+        _spacesBefore -= 1;
+      }
+      if (_scrollAbbrEnd < _scrollAbbrLength) {
+        _scrollAbbrEnd += 1;
+      }
+      if (_scrollAbbrEnd > maxChar) {
+        _scrollAbbrStart += 1;
+      }
+      if ((_scrollAbbrEnd - _scrollAbbrStart + _spacesAfter + _spacesBefore) <
+          maxChar) {
+        _spacesAfter += 1;
+      }
+      if (_spacesAfter >= maxChar - 1) {
+        _isScrollStart = true;
+      }
+    }
+
+    const spaceCharacter = ' ';
+    final r = (spaceCharacter * _spacesBefore) +
+        abbr.substring(_scrollAbbrStart, _scrollAbbrEnd) +
+        (spaceCharacter * _spacesAfter);
+    return r;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _scrollTimer ??= Timer.periodic(Duration(milliseconds: 500), (t) {
+      if (mounted) {
+        final r = coinScroll(widget.abbr);
+        setState(() {
+          text = r;
+        });
+      } else {
+        t.cancel();
+      }
+    });
+
+    return Text(
+      text,
+      style: widget.style,
+      maxLines: widget.maxLines,
+    );
+  }
+
+  @override
+  void dispose() {
+    _scrollTimer.cancel();
+    super.dispose();
   }
 }
