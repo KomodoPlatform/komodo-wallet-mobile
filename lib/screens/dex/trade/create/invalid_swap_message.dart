50d4ca7c303ccc2972a9556c2c7aa8ac25d0da8f
yurii-khi <khiznichenko@gmail.com>
2021-04-11 17:49:31 +0200

    Improve trade_preimage error messages handling

--- lib/screens/dex/trade/create/invalid_swap_message.dart
+++ lib/screens/dex/trade/create/invalid_swap_message.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:komodo_dex/localizations.dart';
 import 'package:komodo_dex/screens/dex/trade/create/trade_form_validator.dart';
+import 'package:komodo_dex/utils/utils.dart';
 
 class InvalidSwapMessage extends StatelessWidget {
   const InvalidSwapMessage(this.apiErrorMessage);
@@ -9,23 +10,26 @@ class InvalidSwapMessage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final BuildContext _mainContext = context;
+
     return InkWell(
       child: Padding(
-        padding: EdgeInsets.fromLTRB(0, 14, 0, 10),
+        padding: EdgeInsets.fromLTRB(12, 14, 12, 10),
         child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
               AppLocalizations.of(context).invalidSwap,
               style: Theme.of(context)
                   .textTheme
-                  .caption
-                  .copyWith(color: Theme.of(context).errorColor),
+                  .bodyText1
+                  .copyWith(color: Colors.orange),
             ),
             SizedBox(width: 2),
             Icon(
               Icons.open_in_new_rounded,
               size: 12,
-              color: Theme.of(context).errorColor,
+              color: Colors.orange,
             )
           ],
         ),
@@ -44,23 +48,33 @@ class InvalidSwapMessage extends StatelessWidget {
                     style: TextStyle(fontSize: 18)),
                 children: [
                   Container(
+                    height: MediaQuery.of(context).size.height * 0.5,
                     padding: EdgeInsets.fromLTRB(0, 16, 0, 8),
-                    child: Column(
-                      children: [
-                        if (validatorErrorMessage != null) ...{
-                          Text(
-                            validatorErrorMessage,
-                            style: TextStyle(fontSize: 13),
-                          ),
-                          SizedBox(height: 10),
-                        },
-                        if (apiErrorMessage != null) ...{
-                          Text(
-                            apiErrorMessage,
-                            style: TextStyle(fontSize: 13),
-                          ),
-                        }
-                      ],
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          if (validatorErrorMessage != null) ...{
+                            Text(
+                              validatorErrorMessage,
+                              style: TextStyle(
+                                fontSize: 13,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                          },
+                          if (apiErrorMessage != null)
+                            InkWell(
+                              onTap: () {
+                                copyToClipBoard(_mainContext, apiErrorMessage);
+                              },
+                              child: Text(
+                                apiErrorMessage,
+                                style: TextStyle(fontSize: 13),
+                              ),
+                            ),
+                        ],
+                      ),
                     ),
                   ),
                   RaisedButton(
