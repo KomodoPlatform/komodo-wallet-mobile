f5f5a595ad91a5de3310021abe375340e5390848
Mateus Rodrigues Costa <mateusrodcosta@gmail.com>
2021-04-22 15:55:14 -0300

    Use SingleChildScrollView for scrolling ticker

--- lib/screens/dex/trade/create/coin_scroll_text.dart
+++ lib/screens/dex/trade/create/coin_scroll_text.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:marquee/marquee.dart';
 
 class CoinScrollText extends StatefulWidget {
   const CoinScrollText(
@@ -17,50 +16,80 @@ class CoinScrollText extends StatefulWidget {
 }
 
 class _CoinScrollTextState extends State<CoinScrollText> {
-  Timer _abbrUpdateTimer;
-  String text = '-';
+  Timer _abbrTimer;
+  String currentAbbr = '-';
+
+  final _scrollController = ScrollController();
+  int speedFactor = 20;
+
+  bool startScroll = true;
+  bool scrollReverse = false;
 
   @override
   void initState() {
     super.initState();
 
-    _abbrUpdateTimer ??= Timer.periodic(Duration(milliseconds: 100), (t) {
-      if (mounted) {
-        if (text != widget.abbr) {
-          setState(() {
-            text = widget.abbr;
-          });
-        }
-      } else {
+    _abbrTimer ??= Timer.periodic(Duration(milliseconds: 100), (t) {
+      if (!mounted) {
         t.cancel();
+        return;
       }
+
+      if (currentAbbr != widget.abbr) {
+        setState(() {
+          currentAbbr = widget.abbr;
+          if (currentAbbr.length > widget.maxChar) {
+            startScroll = true;
+          }
+        });
+        if (currentAbbr.length > widget.maxChar) {
+          _scroll();
+        }
+      }
+    });
+  }
+
+  void _scroll({int delay = 1}) {
+    if (!mounted) return;
+    if (!startScroll) return;
+
+    startScroll = false;
+
+    final maxExtent = _scrollController.position.extentInside;
+    final durationDouble = maxExtent / speedFactor;
+
+    _scrollController
+        .animateTo(scrollReverse ? 0.0 : maxExtent,
+            duration: Duration(seconds: durationDouble.toInt()),
+            curve: Curves.linear)
+        .then((_) {
+      Future.delayed(Duration(seconds: delay), () {
+        scrollReverse = !scrollReverse;
+        startScroll = true;
+      });
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 20,
-      alignment: AlignmentDirectional.center,
-      child: text.length > widget.maxChar
-          ? Marquee(
-              text: text,
-              style: widget.style,
-              blankSpace: 20.0,
-              pauseAfterRound: Duration(seconds: 5),
-              startAfter: Duration(seconds: 2),
-            )
-          : Text(
-              text,
-              style: widget.style,
-              maxLines: widget.maxLines,
-            ),
-    );
+        height: 20,
+        alignment: AlignmentDirectional.center,
+        child: SingleChildScrollView(
+          reverse: scrollReverse,
+          controller: _scrollController,
+          scrollDirection: Axis.horizontal,
+          child: Text(
+            currentAbbr,
+            style: widget.style,
+            maxLines: widget.maxLines,
+          ),
+        ));
   }
 
   @override
   void dispose() {
-    _abbrUpdateTimer.cancel();
+    _abbrTimer.cancel();
     super.dispose();
   }
 }
