1fb511230c4d06726bb04c11ba5afe1622259899
Mateus Rodrigues Costa <mateusrodcosta@gmail.com>
2021-04-24 10:32:18 -0300

    Implement withdraw fiat preview
    Related to #1148

--- lib/screens/portfolio/coin_detail/steps_withdraw.dart/amount_address_step/amount_field.dart
+++ lib/screens/portfolio/coin_detail/steps_withdraw.dart/amount_address_step/amount_field.dart
@@ -2,10 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:komodo_dex/blocs/coin_detail_bloc.dart';
 import 'package:komodo_dex/blocs/coins_bloc.dart';
+import 'package:komodo_dex/blocs/settings_bloc.dart';
 import 'package:komodo_dex/localizations.dart';
+import 'package:komodo_dex/model/cex_provider.dart';
 import 'package:komodo_dex/model/coin_balance.dart';
 import 'package:komodo_dex/screens/dex/trade/trade_form.dart';
 import 'package:komodo_dex/utils/decimal_text_input_formatter.dart';
+import 'package:komodo_dex/widgets/theme_data.dart';
+import 'package:provider/provider.dart';
 
 class AmountField extends StatefulWidget {
   const AmountField(
@@ -28,104 +32,149 @@ class AmountField extends StatefulWidget {
 }
 
 class _AmountFieldState extends State<AmountField> {
+  double amountUsd = 0.0;
+
+  void _calculateFiat(String amount) {
+    final cexProvider = Provider.of<CexProvider>(context, listen: false);
+    final double price = cexProvider.getUsdPrice(widget.coinAbbr);
+
+    final amountParsed = double.tryParse(amount) ?? 0.0;
+
+    final r = amountParsed * price;
+
+    setState(() {
+      amountUsd = r;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    final cexProvider = Provider.of<CexProvider>(context, listen: false);
     return Padding(
       padding: const EdgeInsets.only(bottom: 16),
-      child: Row(
-        children: <Widget>[
-          Container(
-            height: 60,
-            child: ButtonTheme(
-              minWidth: 50,
-              child: FlatButton(
-                padding: const EdgeInsets.only(
-                  left: 6,
-                  right: 6,
+      child: Column(
+        children: [
+          Row(
+            children: <Widget>[
+              Container(
+                height: 60,
+                child: ButtonTheme(
+                  minWidth: 50,
+                  child: FlatButton(
+                    padding: const EdgeInsets.only(
+                      left: 6,
+                      right: 6,
+                    ),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(6.0)),
+                    child: Text(
+                      AppLocalizations.of(context).max,
+                      style: Theme.of(context)
+                          .textTheme
+                          .bodyText2
+                          .copyWith(color: Theme.of(context).accentColor),
+                    ),
+                    onPressed: () {
+                      widget.onMaxValue();
+                      _calculateFiat(widget.controller.text);
+                    },
+                  ),
                 ),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(6.0)),
-                child: Text(
-                  AppLocalizations.of(context).max,
-                  style: Theme.of(context)
-                      .textTheme
-                      .bodyText2
-                      .copyWith(color: Theme.of(context).accentColor),
-                ),
-                onPressed: widget.onMaxValue,
               ),
-            ),
-          ),
-          const SizedBox(
-            width: 8,
-          ),
-          Expanded(
-            child: StreamBuilder<List<CoinBalance>>(
-                initialData: coinsBloc.coinBalance,
-                stream: coinsBloc.outCoins,
-                builder: (BuildContext context,
-                    AsyncSnapshot<List<CoinBalance>> snapshot) {
-                  CoinBalance currentCoinBalance;
-                  if (snapshot.hasData) {
-                    for (CoinBalance coinBalance in snapshot.data) {
-                      if (coinBalance.coin.abbr == widget.coinAbbr) {
-                        currentCoinBalance = coinBalance;
+              const SizedBox(
+                width: 8,
+              ),
+              Expanded(
+                child: StreamBuilder<List<CoinBalance>>(
+                    initialData: coinsBloc.coinBalance,
+                    stream: coinsBloc.outCoins,
+                    builder: (BuildContext context,
+                        AsyncSnapshot<List<CoinBalance>> snapshot) {
+                      CoinBalance currentCoinBalance;
+                      if (snapshot.hasData) {
+                        for (CoinBalance coinBalance in snapshot.data) {
+                          if (coinBalance.coin.abbr == widget.coinAbbr) {
+                            currentCoinBalance = coinBalance;
+                          }
+                        }
                       }
-                    }
-                  }
-                  return TextFormField(
-                    key: const Key('send-amount-field'),
-                    inputFormatters: <TextInputFormatter>[
-                      DecimalTextInputFormatter(
-                          decimalRange: tradeForm.precision),
-                      FilteringTextInputFormatter.allow(RegExp(
-                          '^\$|^(0|([1-9][0-9]{0,12}))([.,]{1}[0-9]{0,8})?\$'))
-                    ],
-                    focusNode: widget.focusNode,
-                    controller: widget.controller,
-                    autofocus: widget.autoFocus,
-                    textInputAction: TextInputAction.done,
-                    keyboardType:
-                        const TextInputType.numberWithOptions(decimal: true),
-                    style: Theme.of(context).textTheme.bodyText2,
-                    textAlign: TextAlign.end,
-                    onChanged: (String amount) {
-                      coinsDetailBloc.setAmountToSend(amount);
-                    },
-                    decoration: InputDecoration(
-                        border: const OutlineInputBorder(),
-                        enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(
-                                color: Theme.of(context).primaryColorLight)),
-                        focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(
-                                color: Theme.of(context).accentColor)),
-                        hintStyle: Theme.of(context).textTheme.bodyText2,
-                        labelStyle: Theme.of(context).textTheme.bodyText2,
-                        labelText: AppLocalizations.of(context).amount),
-                    // The validator receives the text the user has typed in
-                    validator: (String value) {
-                      if (value.isEmpty && coinsDetailBloc.isCancel) {
-                        return null;
-                      }
-                      value = value.replaceAll(',', '.');
+                      return TextFormField(
+                        key: const Key('send-amount-field'),
+                        inputFormatters: <TextInputFormatter>[
+                          DecimalTextInputFormatter(
+                              decimalRange: tradeForm.precision),
+                          FilteringTextInputFormatter.allow(RegExp(
+                              '^\$|^(0|([1-9][0-9]{0,12}))([.,]{1}[0-9]{0,8})?\$'))
+                        ],
+                        focusNode: widget.focusNode,
+                        controller: widget.controller,
+                        autofocus: widget.autoFocus,
+                        textInputAction: TextInputAction.done,
+                        keyboardType: const TextInputType.numberWithOptions(
+                          decimal: true,
+                        ),
+                        style: Theme.of(context).textTheme.bodyText2,
+                        textAlign: TextAlign.end,
+                        onChanged: (String amount) {
+                          coinsDetailBloc.setAmountToSend(amount);
 
-                      if (value.isEmpty || double.parse(value) <= 0) {
-                        return AppLocalizations.of(context).errorValueNotEmpty;
-                      }
+                          _calculateFiat(amount);
+                        },
+                        decoration: InputDecoration(
+                            border: const OutlineInputBorder(),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: Theme.of(context).primaryColorLight),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Theme.of(context).accentColor)),
+                            hintStyle: Theme.of(context).textTheme.bodyText2,
+                            labelStyle: Theme.of(context).textTheme.bodyText2,
+                            labelText: AppLocalizations.of(context).amount),
+                        // The validator receives the text the user has typed in
+                        validator: (String value) {
+                          if (value.isEmpty && coinsDetailBloc.isCancel) {
+                            return null;
+                          }
+                          value = value.replaceAll(',', '.');
 
-                      final double currentAmount = double.parse(value);
+                          if (value.isEmpty || double.parse(value) <= 0) {
+                            return AppLocalizations.of(context)
+                                .errorValueNotEmpty;
+                          }
 
-                      if (currentAmount >
-                          double.parse(
-                              currentCoinBalance.balance.getBalance())) {
-                        return AppLocalizations.of(context).errorAmountBalance;
-                      }
-                      return null;
-                    },
-                  );
-                }),
+                          final double currentAmount = double.parse(value);
+
+                          if (currentAmount >
+                              double.parse(
+                                  currentCoinBalance.balance.getBalance())) {
+                            return AppLocalizations.of(context)
+                                .errorAmountBalance;
+                          }
+                          return null;
+                        },
+                      );
+                    }),
+              ),
+            ],
           ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 8),
+                child: Text(
+                  cexProvider.convert(amountUsd),
+                  style: TextStyle(
+                      fontSize: 14,
+                      color: settingsBloc.isLightTheme
+                          ? cexColorLight.withAlpha(150)
+                          : cexColor.withAlpha(150)),
+                ),
+              ),
+            ],
+          )
         ],
       ),
     );
