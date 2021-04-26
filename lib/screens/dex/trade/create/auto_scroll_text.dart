d66c25d53f8e9d32a40ecb538dea95150c0a274f
Mateus Rodrigues Costa <mateusrodcosta@gmail.com>
2021-04-26 10:53:39 -0300

    Implement auto text scrolling for long strings
    Co-authored-by: Yurii Khiznichenko <yuriikhiznichenko@gmail.com>

--- lib/screens/dex/trade/create/auto_scroll_text.dart
+++ lib/screens/dex/trade/create/auto_scroll_text.dart
@@ -3,45 +3,39 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 
 class AutoScrollText extends StatefulWidget {
-  const AutoScrollText({Key key, this.text, this.style}) : super(key: key);
+  const AutoScrollText({
+    Key key,
+    this.text,
+    this.style,
+    this.delayBefore = const Duration(seconds: 2),
+    this.duration = const Duration(seconds: 1),
+    this.delayBetween = const Duration(milliseconds: 2000),
+  }) : super(key: key);
   final String text;
   final TextStyle style;
+  final Duration delayBefore;
+  final Duration duration;
+  final Duration delayBetween;
 
   @override
   _AutoScrollTextState createState() => _AutoScrollTextState();
 }
 
 class _AutoScrollTextState extends State<AutoScrollText> {
-  Timer _abbrTimer;
-  Timer _scrollTimer;
-
   final _scrollController = ScrollController();
-  int speedFactor = 20;
-
-  bool _shouldScroll = false;
-  bool scrollReverse = false;
-  bool isScrollRunning = false;
-
-  final _textKey = GlobalKey();
-  final _containerKey = GlobalKey();
 
   @override
   void initState() {
     super.initState();
 
-    _initTimers();
-  }
-
-  @override
-  void dispose() {
-    _abbrTimer.cancel();
-    super.dispose();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _animate();
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      key: _containerKey,
       height: 20,
       alignment: AlignmentDirectional.center,
       child: SingleChildScrollView(
@@ -49,52 +43,25 @@ class _AutoScrollTextState extends State<AutoScrollText> {
         scrollDirection: Axis.horizontal,
         child: Text(
           widget.text ?? '',
-          key: _textKey,
           style: widget.style,
         ),
       ),
     );
   }
 
-  Future<void> _scroll({int delay = 1}) async {
-    if (!mounted) return;
-
-    while (_shouldScroll) {
-      isScrollRunning = true;
-      final textWidth = _textKey.currentContext.size.width;
-      await _scrollController.animateTo(scrollReverse ? 0.0 : textWidth,
-          duration: Duration(seconds: scrollReverse ? 1 : 2),
+  Future<void> _animate() async {
+    await Future<dynamic>.delayed(widget.delayBefore);
+    while (true) {
+      await _scrollController.animateTo(
+          _scrollController.position.maxScrollExtent,
+          duration: widget.duration,
           curve: Curves.linear);
-
-      await Future.delayed(Duration(seconds: delay), () {});
-
-      scrollReverse = !scrollReverse;
+      await Future<dynamic>.delayed(Duration(milliseconds: 500));
+      await _scrollController.animateTo(
+          _scrollController.position.minScrollExtent,
+          duration: widget.duration,
+          curve: Curves.linear);
+      await Future<dynamic>.delayed(widget.delayBetween);
     }
-    isScrollRunning = false;
-  }
-
-  void _initTimers() {
-    _abbrTimer ??= Timer.periodic(Duration(milliseconds: 100), (t) {
-      if (!mounted) {
-        t.cancel();
-        return;
-      }
-
-      setState(() {
-        _shouldScroll = false;
-
-        _scrollTimer?.cancel();
-
-        _scrollTimer ??= Timer.periodic(Duration(milliseconds: 500), (t) async {
-          if (!_shouldScroll) {
-            final textWidth = _textKey.currentContext.size.width;
-            final containerWidth = _containerKey.currentContext.size.width;
-
-            _shouldScroll = textWidth > containerWidth;
-          }
-          if (!isScrollRunning) _scroll();
-        });
-      });
-    });
   }
 }
