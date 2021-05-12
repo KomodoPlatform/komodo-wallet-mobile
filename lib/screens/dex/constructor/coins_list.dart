f3602af1d6e6ac5be4da854280e578c0723e89a7
yurii-khi <khiznichenko@gmail.com>
2021-05-12 09:18:35 +0200

    Move coins list to separate widget

---
+++ lib/screens/dex/constructor/coins_list.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:komodo_dex/blocs/coins_bloc.dart';
+import 'package:komodo_dex/model/coin.dart';
+import 'package:komodo_dex/model/order_book_provider.dart';
+import 'package:komodo_dex/model/orderbook_depth.dart';
+import 'package:komodo_dex/screens/markets/coin_select.dart';
+import 'package:provider/provider.dart';
+
+class CoinsList extends StatefulWidget {
+  const CoinsList({this.type});
+
+  final CoinType type;
+
+  @override
+  _CoinsListState createState() => _CoinsListState();
+}
+
+class _CoinsListState extends State<CoinsList> {
+  OrderBookProvider _obProvider;
+
+  @override
+  Widget build(BuildContext context) {
+    _obProvider ??= Provider.of<OrderBookProvider>(context);
+    final List<Coin> coins = _getCoins();
+
+    return ListView.builder(
+      itemCount: coins.length,
+      itemBuilder: (context, i) {
+        return _buildCoinItem(coins[i]);
+      },
+    );
+  }
+
+  Widget _buildCoinItem(Coin coin) {
+    return Card(
+      margin: EdgeInsets.fromLTRB(0, 6, 12, 0),
+      child: Container(
+          padding: EdgeInsets.fromLTRB(8, 10, 8, 10),
+          child: Row(
+            children: [
+              CircleAvatar(
+                radius: 8,
+                backgroundImage:
+                    AssetImage('assets/${coin.abbr.toLowerCase()}.png'),
+              ),
+              SizedBox(width: 4),
+              Text(
+                coin.abbr,
+                style: Theme.of(context).textTheme.subtitle1,
+              ),
+              Expanded(child: SizedBox()),
+              Text(
+                _getMatchingCoinsNumber(coin).toString(),
+                style: Theme.of(context).textTheme.caption,
+              )
+            ],
+          )),
+    );
+  }
+
+  int _getMatchingCoinsNumber(Coin coin) {
+    int counter = 0;
+    final List<OrderbookDepth> obDepths = _obProvider.depthsForCoin(coin);
+    for (OrderbookDepth obDepth in obDepths) {
+      final String requiredBalanceCoin =
+          widget.type == CoinType.base ? obDepth.pair.base : obDepth.pair.rel;
+      final double requiredBalance = coinsBloc
+              .getBalanceByAbbr(requiredBalanceCoin)
+              ?.balance
+              ?.balance
+              ?.toDouble() ??
+          0;
+      if (requiredBalance == 0) continue;
+
+      final int matchingOrders = widget.type == CoinType.base
+          ? obDepth.depth.bids
+          : obDepth.depth.asks;
+      if (matchingOrders > 0) counter++;
+    }
+    return counter;
+  }
+
+  List<Coin> _getCoins() {
+    final List<Coin> active =
+        coinsBloc.coinBalance.map((bal) => bal.coin).toList();
+
+    final List<Coin> available = [];
+    for (Coin coin in active) {
+      if (_getMatchingCoinsNumber(coin) == 0) continue;
+      available.add(coin);
+    }
+
+    available.sort((a, b) {
+      final int aNum = _getMatchingCoinsNumber(a);
+      final int bNum = _getMatchingCoinsNumber(b);
+      if (aNum > bNum) return -1;
+      if (aNum < bNum) return 1;
+
+      return a.abbr.compareTo(b.abbr);
+    });
+
+    return available;
+  }
+}
--- lib/screens/dex/constructor/constructor.dart
+++ lib/screens/dex/constructor/constructor.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:komodo_dex/blocs/coins_bloc.dart';
 import 'package:komodo_dex/model/coin.dart';
 import 'package:komodo_dex/model/order_book_provider.dart';
-import 'package:komodo_dex/model/orderbook_depth.dart';
+import 'package:komodo_dex/screens/dex/constructor/coins_list.dart';
 import 'package:komodo_dex/screens/markets/coin_select.dart';
 import 'package:provider/provider.dart';
 
@@ -58,7 +58,7 @@ class _SwapConstructorState extends State<SwapConstructor> {
           style: Theme.of(context).textTheme.subtitle2,
         ),
         SizedBox(height: 6),
-        Expanded(child: _buildCoinsList(CoinType.base)),
+        Expanded(child: CoinsList(type: CoinType.base)),
       ],
     );
   }
@@ -72,31 +72,9 @@ class _SwapConstructorState extends State<SwapConstructor> {
           style: Theme.of(context).textTheme.subtitle2,
         ),
         SizedBox(height: 6),
-        Expanded(child: _buildCoinsList(CoinType.rel)),
+        Expanded(child: CoinsList(type: CoinType.rel)),
       ],
     );
-  }
-
-  List<Coin> _getCoins(CoinType type) {
-    final List<Coin> active =
-        coinsBloc.coinBalance.map((bal) => bal.coin).toList();
-
-    final List<Coin> available = [];
-    for (Coin coin in active) {
-      if (_getMatchingCoinsNumber(coin, type) == 0) continue;
-      available.add(coin);
-    }
-
-    available.sort((a, b) {
-      final int aNum = _getMatchingCoinsNumber(a, type);
-      final int bNum = _getMatchingCoinsNumber(b, type);
-      if (aNum > bNum) return -1;
-      if (aNum < bNum) return 1;
-
-      return a.abbr.compareTo(b.abbr);
-    });
-
-    return available;
   }
 
   Widget _buildProgress() {
@@ -106,65 +84,6 @@ class _SwapConstructorState extends State<SwapConstructor> {
         child: CircularProgressIndicator(),
       ),
     );
-  }
-
-  Widget _buildCoinsList(CoinType type) {
-    final List<Coin> coins = _getCoins(type);
-
-    return ListView.builder(
-      itemCount: coins.length,
-      itemBuilder: (context, i) {
-        return _buildCoinItem(coins[i], type);
-      },
-    );
-  }
-
-  Widget _buildCoinItem(Coin coin, CoinType type) {
-    return Card(
-      margin: EdgeInsets.fromLTRB(0, 6, 12, 0),
-      child: Container(
-          padding: EdgeInsets.fromLTRB(8, 10, 8, 10),
-          child: Row(
-            children: [
-              CircleAvatar(
-                radius: 8,
-                backgroundImage:
-                    AssetImage('assets/${coin.abbr.toLowerCase()}.png'),
-              ),
-              SizedBox(width: 4),
-              Text(
-                coin.abbr,
-                style: Theme.of(context).textTheme.subtitle1,
-              ),
-              Expanded(child: SizedBox()),
-              Text(
-                _getMatchingCoinsNumber(coin, type).toString(),
-                style: Theme.of(context).textTheme.caption,
-              )
-            ],
-          )),
-    );
-  }
-
-  int _getMatchingCoinsNumber(Coin coin, CoinType type) {
-    int counter = 0;
-    final List<OrderbookDepth> obDepths = _obProvider.depthsForCoin(coin);
-    for (OrderbookDepth obDepth in obDepths) {
-      final String requiredBalanceCoin =
-          type == CoinType.base ? obDepth.pair.base : obDepth.pair.rel;
-      final double requiredBalance = coinsBloc
-              .getBalanceByAbbr(requiredBalanceCoin)
-              ?.balance
-              ?.balance
-              ?.toDouble() ??
-          0;
-      if (requiredBalance == 0) continue;
-
-      final int matchingOrders =
-          type == CoinType.base ? obDepth.depth.bids : obDepth.depth.asks;
-      if (matchingOrders > 0) counter++;
-    }
-    return counter;
   }
 
   Future<bool> _subscribeDepths() async {
