a4ad745ca85d1a5312f729aec4871f29505e27dc
Mateus Rodrigues Costa <mateusrodcosta@gmail.com>
2021-05-22 10:27:17 -0300

    WIP:  Use RepaintBoundary to capture app screen

--- lib/screens/dex/orders/swap/final_trade_success.dart
+++ lib/screens/dex/orders/swap/final_trade_success.dart
@@ -1,9 +1,14 @@
+import 'dart:io';
+import 'dart:ui' as ui;
+
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:komodo_dex/localizations.dart';
 import 'package:komodo_dex/model/swap.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:komodo_dex/screens/dex/orders/swap/detail_swap.dart';
 import 'package:komodo_dex/blocs/settings_bloc.dart';
+import 'package:komodo_dex/utils/utils.dart';
 
 class FinalTradeSuccess extends StatefulWidget {
   const FinalTradeSuccess({@required this.swap});
@@ -18,6 +23,8 @@ class _FinalTradeSuccessState extends State<FinalTradeSuccess>
     with SingleTickerProviderStateMixin {
   AnimationController animationController;
   Animation<dynamic> animation;
+
+  GlobalKey repaintKey = GlobalKey();
 
   @override
   void initState() {
@@ -42,48 +49,78 @@ class _FinalTradeSuccessState extends State<FinalTradeSuccess>
   Widget build(BuildContext context) {
     return FadeTransition(
       opacity: animationController.drive(CurveTween(curve: Curves.easeOut)),
-      child: Center(
-        child: ListView(
-          children: <Widget>[
-            const SizedBox(
-              height: 32,
-            ),
-            Container(
-              height: 200,
-              child: SvgPicture.asset(
-                  settingsBloc.isLightTheme
-                      ? 'assets/svg_light/trade_success.svg'
-                      : 'assets/svg/trade_success.svg',
-                  semanticsLabel: 'Trade Success'),
-            ),
-            const SizedBox(
-              height: 32,
-            ),
-            Column(
-              children: <Widget>[
-                Text(AppLocalizations.of(context).trade,
-                    style: Theme.of(context).textTheme.headline6),
-                Text(
-                  AppLocalizations.of(context).tradeCompleted,
-                  style: Theme.of(context)
-                      .textTheme
-                      .headline6
-                      .copyWith(color: Theme.of(context).accentColor),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 32,
-            ),
-            Container(
-              color: const Color.fromARGB(255, 52, 62, 76),
-              height: 1,
-              width: double.infinity,
-            ),
-            DetailSwap(
-              swap: widget.swap,
-            )
-          ],
+      child: RepaintBoundary(
+        key: repaintKey,
+        child: Center(
+          child: ListView(
+            children: <Widget>[
+              Stack(
+                children: [
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: IconButton(
+                      icon: Icon(Icons.share),
+                      onPressed: () async {
+                        final RenderRepaintBoundary boundary =
+                            repaintKey.currentContext.findRenderObject();
+                        final ui.Image image = await boundary.toImage();
+                        final byteData = await image.toByteData(
+                            format: ui.ImageByteFormat.png);
+                        final pngBytes = byteData.buffer.asUint8List();
+
+                        final String directory =
+                            (await applicationDocumentsDirectory).path;
+                        final imgFile = File('$directory/screenshot.png');
+                        await imgFile.writeAsBytes(pngBytes);
+                      },
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      const SizedBox(
+                        height: 32,
+                      ),
+                      Container(
+                        height: 200,
+                        child: SvgPicture.asset(
+                            settingsBloc.isLightTheme
+                                ? 'assets/svg_light/trade_success.svg'
+                                : 'assets/svg/trade_success.svg',
+                            semanticsLabel: 'Trade Success'),
+                      ),
+                      const SizedBox(
+                        height: 32,
+                      ),
+                      Column(
+                        children: <Widget>[
+                          Text(AppLocalizations.of(context).trade,
+                              style: Theme.of(context).textTheme.headline6),
+                          Text(
+                            AppLocalizations.of(context).tradeCompleted,
+                            style: Theme.of(context)
+                                .textTheme
+                                .headline6
+                                .copyWith(color: Theme.of(context).accentColor),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 32,
+                      ),
+                      Container(
+                        color: const Color.fromARGB(255, 52, 62, 76),
+                        height: 1,
+                        width: double.infinity,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              DetailSwap(
+                swap: widget.swap,
+              )
+            ],
+          ),
         ),
       ),
     );
