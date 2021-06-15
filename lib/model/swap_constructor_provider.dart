e00d5e13be36f335cb2e1df00ee5273f5040b8dc
yurii-khi <khiznichenko@gmail.com>
2021-06-15 09:06:10 +0200

    WIP: Add max sell amt getter

--- lib/model/swap_constructor_provider.dart
+++ lib/model/swap_constructor_provider.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:komodo_dex/blocs/coins_bloc.dart';
+import 'package:komodo_dex/model/coin_balance.dart';
 
 class ConstructorProvider extends ChangeNotifier {
   String _sellCoin;
   String _buyCoin;
+  double _sellAmount;
 
   String get sellCoin => _sellCoin;
   set sellCoin(String value) {
@@ -14,5 +17,22 @@ class ConstructorProvider extends ChangeNotifier {
   set buyCoin(String value) {
     _buyCoin = value;
     notifyListeners();
+  }
+
+  double get sellAmount => _sellAmount;
+  set sellAmount(double value) {
+    _sellAmount = value;
+    notifyListeners();
+  }
+
+  double maxSellAmt() {
+    /// todo: implement max balance calculation,
+    /// considering fees, max_taker_volume(?), preimage etc.
+    if (_sellCoin == null) return null;
+
+    final CoinBalance coinBalance = coinsBloc.getBalanceByAbbr(_sellCoin);
+    if (coinBalance == null) return null;
+
+    return coinBalance.balance.balance.toDouble();
   }
 }
