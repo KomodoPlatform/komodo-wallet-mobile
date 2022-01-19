b220430d3c69081068913fcf3c5fd89ed602887f
yurii-khi <yuriikhiznichenko@gmail.com>
2022-01-19 09:48:53 +0100

    Improve trading modes switcher

--- lib/screens/dex/dex_page.dart
+++ lib/screens/dex/dex_page.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:komodo_dex/blocs/main_bloc.dart';
 import 'package:komodo_dex/blocs/orders_bloc.dart';
 import 'package:komodo_dex/blocs/swap_bloc.dart';
 import 'package:komodo_dex/localizations.dart';
 import 'package:komodo_dex/screens/dex/orders/orders_page.dart';
-import 'package:komodo_dex/screens/dex/trade/trade_mode_indicator.dart';
 import 'package:komodo_dex/screens/dex/trade/trade_modes_page.dart';
 import 'package:komodo_dex/utils/custom_tab_indicator.dart';
 import 'package:komodo_dex/utils/utils.dart';
@@ -16,38 +14,17 @@ class DexPage extends StatefulWidget {
 
 class _DexPageState extends State<DexPage> with TickerProviderStateMixin {
   TabController tabController;
-  TabController _tradeTabController;
-  bool _showTradeMode = false;
 
   @override
   void initState() {
     super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
-      setState(() {
-        _showTradeMode = tabController.index == 0;
-      });
-    });
 
     tabController = TabController(length: 2, vsync: this);
-    tabController.addListener(() {
-      setState(() {
-        _showTradeMode = tabController.index == 0;
-      });
-    });
 
     ordersBloc.updateOrdersSwaps();
 
     swapBloc.outIndexTab.listen((int onData) {
       if (mounted) setState(() => tabController.index = onData);
-    });
-
-    _tradeTabController = TabController(
-      length: 3,
-      vsync: this,
-      initialIndex: mainBloc.tradeMode,
-    );
-    _tradeTabController.addListener(() {
-      mainBloc.tradeMode = _tradeTabController.index;
     });
   }
 
@@ -81,8 +58,8 @@ class _DexPageState extends State<DexPage> with TickerProviderStateMixin {
       );
 
       return AppBar(
+        elevation: 4,
         title: _tabsPanel,
-        bottom: _showTradeMode ? _buildModeSwitcher() : null,
         automaticallyImplyLeading: false,
       );
     }
@@ -105,45 +82,6 @@ class _DexPageState extends State<DexPage> with TickerProviderStateMixin {
               ],
             );
           }),
-        ),
-      ),
-    );
-  }
-
-  // todo(MRC): Cosnider moving this to its own widget
-  // I had to move it here so to try to properly integrate it with the app bar
-  // by using its "bottom" porperty, otherwise I wouldn't be able to make it
-  // have the same color as the app bar
-
-  Widget _buildModeSwitcher() {
-    return PreferredSize(
-      preferredSize: Size.fromHeight(kToolbarHeight),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: Row(
-          children: [
-            Expanded(
-                child: Text(
-              'Trading Mode:',
-              style: Theme.of(context).textTheme.bodyText1.copyWith(
-                    color: Theme.of(context).colorScheme.onPrimary,
-                  ),
-            )),
-            Flexible(
-              flex: 2,
-              child: TabBar(
-                controller: _tradeTabController,
-                indicator: TradeModeIndicator(context: context),
-                labelPadding: EdgeInsets.symmetric(horizontal: 0),
-                indicatorPadding: EdgeInsets.symmetric(horizontal: 0),
-                tabs: [
-                  Tab(text: 'Simple'),
-                  Tab(text: 'Advanced'),
-                  Tab(text: AppLocalizations.of(context).multiTab),
-                ],
-              ),
-            ),
-          ],
         ),
       ),
     );
--- lib/screens/dex/trade/trade_modes_page.dart
+++ lib/screens/dex/trade/trade_modes_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:komodo_dex/blocs/main_bloc.dart';
+import 'package:komodo_dex/localizations.dart';
 import 'package:komodo_dex/screens/dex/trade/multi/multi_order_page.dart';
 import 'package:komodo_dex/screens/dex/trade/pro/create/trade_page.dart';
 import 'package:komodo_dex/screens/dex/trade/simple/trade_page_simple.dart';
+import 'package:komodo_dex/screens/dex/trade/trade_mode_indicator.dart';
 
 class TradeModesPage extends StatefulWidget {
   @override
@@ -13,12 +15,73 @@ class _TradeModesPageState extends State<TradeModesPage>
     with TickerProviderStateMixin, AutomaticKeepAliveClientMixin {
   @override
   bool get wantKeepAlive => true;
+  TabController _tradeTabController;
+
+  @override
+  void initState() {
+    _tradeTabController = TabController(
+      length: 3,
+      vsync: this,
+      initialIndex: mainBloc.tradeMode,
+    );
+    _tradeTabController.addListener(() {
+      mainBloc.tradeMode = _tradeTabController.index;
+    });
+
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     super.build(context);
 
-    return _buildView();
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      children: [
+        _buildSwitcher(),
+        Expanded(child: _buildView()),
+      ],
+    );
+  }
+
+  Widget _buildSwitcher() {
+    final style = Theme.of(context)
+        .textTheme
+        .caption
+        .copyWith(color: Theme.of(context).colorScheme.onPrimary);
+
+    return PreferredSize(
+      preferredSize: Size.fromHeight(kToolbarHeight),
+      child: Container(
+        color: Theme.of(context).colorScheme.surface,
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Row(
+          children: [
+            Expanded(
+                child: Text(
+              'Trading Mode:',
+              style: style,
+            )),
+            Flexible(
+              flex: 2,
+              child: TabBar(
+                controller: _tradeTabController,
+                indicator: TradeModeIndicator(context: context),
+                labelPadding: EdgeInsets.symmetric(horizontal: 0),
+                indicatorPadding: EdgeInsets.symmetric(horizontal: 0),
+                tabs: [
+                  Tab(child: Text('Simple', style: style)),
+                  Tab(child: Text('Advanced', style: style)),
+                  Tab(
+                      child: Text(AppLocalizations.of(context).multiTab,
+                          style: style)),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 
   Widget _buildView() {
