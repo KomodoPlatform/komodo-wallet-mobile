27d5dc90df7e50eb0262ff9b33136b4c64cc4f3f
Mateus Rodrigues Costa <mateusrodcosta@gmail.com>
2022-03-14 19:25:22 -0300

    Fix linter warning

--- lib/screens/portfolio/coin_detail/coin_detail.dart
+++ lib/screens/portfolio/coin_detail/coin_detail.dart
@@ -31,7 +31,6 @@ import 'package:komodo_dex/services/mm_service.dart';
 import 'package:komodo_dex/utils/utils.dart';
 import 'package:komodo_dex/widgets/auto_scroll_text.dart';
 import 'package:komodo_dex/widgets/build_red_dot.dart';
-import 'package:komodo_dex/widgets/custom_simple_dialog.dart';
 import 'package:komodo_dex/widgets/photo_widget.dart';
 import 'package:komodo_dex/widgets/secondary_button.dart';
 import 'package:provider/provider.dart';
