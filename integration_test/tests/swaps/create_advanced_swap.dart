3c3ce9170b0520ffc41ad9550b789e049117c863
Temitope Ologunbaba <temitope@glade.ng>
2022-06-24 12:06:04 +0100

    Refactor advanced swap test

--- integration_test/tests/swaps/create_advanced_swap.dart
+++ integration_test/tests/swaps/create_advanced_swap.dart
@@ -1,46 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:komodo_dex/main.dart';
+import 'package:komodo_dex/screens/dex/dex_page.dart';
 
 Future<void> createAdvancedSwap(WidgetTester tester) async {
+  final Finder dexMenu = find.byKey(const Key('main-nav-dex'));
+  final Finder advancedTabBtn = find.byKey(const Key('advanced-tab'));
+
+  final Finder clearBtn = find.byKey(const Key('clear-trade-button'));
+  final Finder nextBtn = find.byKey(const Key('trade-button'));
+  final Finder cancelBtn = find.byKey(const Key('cancel-swap-button'));
+  final Finder startSwapBtn = find.byKey(Key('confirm-swap-button'));
+  final Finder backBtn = find.byTooltip('Back');
+  final Finder orderTabBtn = find.byKey(const Key('orders-tab'));
+  final Finder activeOrderList = find.byKey(const Key('active-order-list'));
+
   try {
-    final Finder dexMenu = find.byKey(const Key('main-nav-dex'));
-    final Finder advancedTabBtn = find.byKey(const Key('advanced-tab'));
-    final Finder openSellDialog = find.byKey(const Key('coin-select-sell'));
-    final Finder sellMortyCoin =
-        find.byKey(const Key('item-dialog-morty-sell'));
-    final Finder sellAmountField = find.byKey(const Key('input-text-sell'));
-    final Finder openReceiveDialog = find.byKey(const Key('coin-select-buy'));
-    final Finder buyRickCoin = find.byKey(const Key('orderbook-item-rick'));
-    final Finder selectOrder = find.byKey(const Key('ask-item-0'));
-    final Finder confirmOrder = find.byKey(const Key('confirm-details'));
-    final Finder clearBtn = find.byKey(const Key('clear-trade-button'));
-    final Finder nextBtn = find.byKey(const Key('trade-button'));
-    final Finder cancelBtn = find.byKey(const Key('cancel-swap-button'));
-    final Finder startSwapBtn = find.byKey(Key('confirm-swap-button'));
-    final Finder backBtn = find.byTooltip('Back');
-    final Finder orderTabBtn = find.byKey(const Key('orders-tab'));
+    expect(
+      find.byType(MyHomePage),
+      findsOneWidget,
+      reason: 'Screen is not on the Home page',
+    );
 
     // Press dex menu
     await tester.tap(dexMenu);
     await tester.pumpAndSettle();
     await tester.tap(advancedTabBtn);
+    await tester.pumpAndSettle();
+    expect(
+      find.byType(DexPage),
+      findsOneWidget,
+      reason: 'Screen is not on the Dex page',
+    );
 
     // sell aspect
-    await tester.pumpAndSettle();
-    await tester.tap(openSellDialog);
-    await tester.pumpAndSettle();
-    await tester.tap(sellMortyCoin);
-    await tester.pumpAndSettle();
-    await tester.enterText(sellAmountField, '0.1');
-    await tester.pumpAndSettle();
+    await _sellAspect(tester, amount: '0.1');
 
     // receive aspect
-    await tester.tap(openReceiveDialog);
-    await tester.pumpAndSettle();
-    await tester.tap(buyRickCoin);
-    await tester.pumpAndSettle();
-    await tester.longPress(selectOrder);
-    await tester.pumpAndSettle(Duration(seconds: 1));
+    await _receiveAspect(tester, confirm: false);
 
     // proceed, cancel and clear fields
     await tester.tap(nextBtn);
@@ -51,23 +48,10 @@ Future<void> createAdvancedSwap(WidgetTester tester) async {
     await tester.pumpAndSettle();
 
     // sell aspect
-    await tester.pumpAndSettle();
-    await tester.tap(openSellDialog);
-    await tester.pumpAndSettle();
-    await tester.tap(sellMortyCoin);
-    await tester.pumpAndSettle();
-    await tester.enterText(sellAmountField, '0.2');
-    await tester.pumpAndSettle();
+    await _sellAspect(tester, amount: '0.2');
 
     // receive aspect
-    await tester.tap(openReceiveDialog);
-    await tester.pumpAndSettle();
-    await tester.tap(buyRickCoin);
-    await tester.pumpAndSettle();
-    await tester.tap(selectOrder);
-    await tester.pumpAndSettle();
-    await tester.tap(confirmOrder);
-    await tester.pumpAndSettle();
+    await _receiveAspect(tester);
 
     // proceed and confirm order
     await tester.tap(nextBtn);
@@ -78,10 +62,65 @@ Future<void> createAdvancedSwap(WidgetTester tester) async {
     // go back to order page to see order
     await tester.tap(backBtn);
     await tester.pumpAndSettle();
+    expect(
+      find.byType(DexPage),
+      findsOneWidget,
+      reason: 'Screen is not on the Dex page',
+    );
     await tester.tap(orderTabBtn);
     await tester.pumpAndSettle();
+    expect(
+      tester.widgetList<ListView>(activeOrderList).length,
+      1,
+      reason: 'Order was not created',
+    );
     // Order has been created
   } catch (e) {
     print(e?.message ?? e);
+    rethrow;
   }
+}
+
+Future<void> _sellAspect(WidgetTester tester, {String amount}) async {
+  final Finder openSellDialog = find.byKey(const Key('coin-select-sell'));
+  final Finder sellMortyCoin = find.byKey(const Key('item-dialog-morty-sell'));
+  final Finder sellAmountField = find.byKey(const Key('input-text-sell'));
+
+  await tester.tap(openSellDialog);
+  await tester.pumpAndSettle();
+  expect(
+    find.byKey(Key('sell-coin-dialog')),
+    findsOneWidget,
+    reason: 'Sell dialog is not on Opened',
+  );
+  await tester.tap(sellMortyCoin);
+  await tester.pumpAndSettle();
+  await tester.enterText(sellAmountField, amount);
+  await tester.pumpAndSettle();
+}
+
+Future<void> _receiveAspect(WidgetTester tester, {bool confirm = true}) async {
+  final Finder openReceiveDialog = find.byKey(const Key('coin-select-buy'));
+  final Finder buyRickCoin = find.byKey(const Key('orderbook-item-rick'));
+  final Finder selectOrder = find.byKey(const Key('ask-item-0'));
+  final Finder confirmOrder = find.byKey(const Key('confirm-details'));
+
+  await tester.tap(openReceiveDialog);
+  await tester.pumpAndSettle();
+  expect(
+    find.byKey(Key('buy-coin-dialog')),
+    findsOneWidget,
+    reason: 'Receive dialog is not on Opened',
+  );
+  await tester.tap(buyRickCoin);
+  await tester.pumpAndSettle();
+  if (!confirm) {
+    await tester.longPress(selectOrder);
+  } else {
+    await tester.tap(selectOrder);
+    await tester.pumpAndSettle();
+    await tester.tap(confirmOrder);
+    await tester.pumpAndSettle();
+  }
+  await tester.pumpAndSettle(Duration(seconds: 1));
 }
--- integration_test/tests/swaps/swap_tests.dart
+++ integration_test/tests/swaps/swap_tests.dart
@@ -2,20 +2,20 @@ import 'package:komodo_dex/main.dart' as app;
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
-import '../wallets_tests/restore_wallet.dart';
+import '../wallet_tests/restore_wallet.dart';
 import 'activate_test_coins.dart';
 import 'create_simple_swap.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  const String firstWalletName = 'my-wallet';
 
   testWidgets('Run swap tests:', (WidgetTester tester) async {
     tester.testTextInput.register();
     app.main();
-    // delay for splash screen and checking updates
     await tester.pumpAndSettle();
     print('RESTORE WALLET TO TEST');
-    await restoreWalletToTest(tester, fromStart: false);
+    await restoreWalletToTest(tester, existingWalletName: firstWalletName);
     await tester.pumpAndSettle();
     print('ACTIVATE TEST COINS');
     await activateTestCoins(tester);
