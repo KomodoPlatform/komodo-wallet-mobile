ba86ab7f03c1033f373e47dd852a611059a606d4
Temitope Ologunbaba <temitope@glade.ng>
2022-06-24 12:08:32 +0100

    Refactor simple swap test

--- integration_test/runners/cancel_swap.dart
+++ integration_test/runners/cancel_swap.dart
@@ -4,10 +4,11 @@ import 'package:integration_test/integration_test.dart';
 
 import '../tests/swaps/activate_test_coins.dart';
 import '../tests/swaps/cancel_swap.dart';
-import '../tests/wallets_tests/restore_wallet.dart';
+import '../tests/wallet_tests/restore_wallet.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  const String firstWalletName = 'my-wallet';
 
   testWidgets('Cancel Swap test', (WidgetTester tester) async {
     tester.testTextInput.register();
@@ -15,7 +16,7 @@ void main() {
     // delay for splash screen and checking updates
     await tester.pumpAndSettle();
     print('RESTORE WALLET TO TEST');
-    await restoreWalletToTest(tester);
+    await restoreWalletToTest(tester, existingWalletName: firstWalletName);
     await tester.pumpAndSettle();
     print('ACTIVATE TEST COINS');
     await activateTestCoins(tester);
