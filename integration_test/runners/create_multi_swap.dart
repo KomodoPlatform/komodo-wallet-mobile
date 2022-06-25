aebef4941bed1b0e7a6b850e6d8eb79bcec8ae6b
Temitope Ologunbaba <temitope@glade.ng>
2022-06-25 11:20:12 +0100

    Add 'create Multi swap' test

--- integration_test/runners/create_multi_swap.dart
+++ integration_test/runners/create_multi_swap.dart
@@ -2,26 +2,31 @@ import 'package:komodo_dex/main.dart' as app;
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
-import '../wallet_tests/restore_wallet.dart';
-import 'activate_test_coins.dart';
-import 'create_simple_swap.dart';
+import '../tests/swaps/activate_test_coins.dart';
+import '../tests/swaps/create_multi_swap.dart';
+import '../tests/swaps/enable_test_coins.dart';
+import '../tests/wallet_tests/restore_wallet.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
   const String firstWalletName = 'my-wallet';
 
-  testWidgets('Run swap tests:', (WidgetTester tester) async {
+  testWidgets('Create a multi Swap test', (WidgetTester tester) async {
     tester.testTextInput.register();
     app.main();
+    // delay for splash screen and checking updates
     await tester.pumpAndSettle();
     print('RESTORE WALLET TO TEST');
     await restoreWalletToTest(tester, existingWalletName: firstWalletName);
     await tester.pumpAndSettle();
-    print('ACTIVATE TEST COINS');
-    await activateTestCoins(tester);
+    print('ENABLE TEST COINS');
+    await enableTestCoin(tester);
     await tester.pumpAndSettle();
-    print('CREATE A SWAP');
-    await createSimpleSwap(tester);
+    print('ACTIVATE TEST COINS');
+    await activateTestCoins(tester, list: ['MORTY', 'RICK', 'tQTUM']);
+    await tester.pumpAndSettle();
+    print('CREATE AN MULTI SWAP');
+    await createMultiSwap(tester);
     await tester.pumpAndSettle();
   }, semanticsEnabled: false);
 }
