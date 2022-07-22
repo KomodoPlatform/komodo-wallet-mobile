9c4da0ec42b2f6a7491663c81c4dc675bd591c5c
Temitope Ologunbaba <temitope@glade.ng>
2022-07-22 18:37:08 +0100

    Add empty state for markets page

--- lib/screens/markets/coins_price_list.dart
+++ lib/screens/markets/coins_price_list.dart
@@ -5,6 +5,8 @@ import 'package:komodo_dex/model/coin_balance.dart';
 import 'package:komodo_dex/screens/markets/build_coin_price_list_item.dart';
 import 'package:komodo_dex/services/mm_service.dart';
 
+import '../../localizations.dart';
+
 class CoinsPriceList extends StatefulWidget {
   const CoinsPriceList({this.onItemTap});
 
@@ -33,34 +35,41 @@ class _CoinsPriceListState extends State<CoinsPriceList> {
         if (snapshot.data != null && snapshot.data.isNotEmpty) {
           final List<CoinBalance> _sortedList =
               coinsBloc.sortCoins(snapshot.data, removeTestCoins: true);
-          return Listener(
-            onPointerDown: (_) {
-              setState(() {
-                touchCounter++;
-              });
-            },
-            onPointerUp: (_) {
-              setState(() {
-                touchCounter--;
-              });
-            },
-            child: ListView.builder(
-                physics: touchCounter > 1
-                    ? const NeverScrollableScrollPhysics()
-                    : const ScrollPhysics(),
-                shrinkWrap: true,
-                itemCount: _sortedList.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return BuildCoinPriceListItem(
-                    key: Key(
-                        'coin-' + _sortedList[index].coin.abbr.toUpperCase()),
-                    coinBalance: _sortedList[index],
-                    onTap: () {
-                      widget.onItemTap(_sortedList[index].coin);
-                    },
-                  );
-                }),
-          );
+          return _sortedList.isEmpty
+              ? Center(
+                  child: Text(
+                    AppLocalizations.of(context).noCoinFound,
+                    style: TextStyle(fontSize: 18),
+                  ),
+                )
+              : Listener(
+                  onPointerDown: (_) {
+                    setState(() {
+                      touchCounter++;
+                    });
+                  },
+                  onPointerUp: (_) {
+                    setState(() {
+                      touchCounter--;
+                    });
+                  },
+                  child: ListView.builder(
+                      physics: touchCounter > 1
+                          ? const NeverScrollableScrollPhysics()
+                          : const ScrollPhysics(),
+                      shrinkWrap: true,
+                      itemCount: _sortedList.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return BuildCoinPriceListItem(
+                          key: Key('coin-' +
+                              _sortedList[index].coin.abbr.toUpperCase()),
+                          coinBalance: _sortedList[index],
+                          onTap: () {
+                            widget.onItemTap(_sortedList[index].coin);
+                          },
+                        );
+                      }),
+                );
         } else {
           return const Center(
             child: CircularProgressIndicator(),
