2e90c9a59b598946ce75e1964e8c47f6e2f78d2d
Yurii Khiznichenko <khiznichenko@gmail.com>
2022-07-22 13:50:30 +0200

    WIP: refactor protocols menu dialog

--- lib/screens/portfolio/activate/build_filter_coin.dart
+++ lib/screens/portfolio/activate/build_filter_coin.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:komodo_dex/blocs/dialog_bloc.dart';
 import 'package:komodo_dex/screens/portfolio/activate/show_protocol_menu.dart'
     as menu;
 import 'package:komodo_dex/utils/utils.dart';
@@ -22,6 +23,8 @@ class BuildFilterCoin extends StatefulWidget {
 
 class _BuildFilterCoinState extends State<BuildFilterCoin> {
   static Map _typesName = {};
+  final GlobalKey globalKey = GlobalKey();
+
   @override
   void initState() {
     readJson();
@@ -38,57 +41,80 @@ class _BuildFilterCoinState extends State<BuildFilterCoin> {
   @override
   Widget build(BuildContext context) {
     return Column(
+      key: globalKey,
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Padding(
           padding: EdgeInsets.only(right: 8.0, left: 4),
-          child: menu.CustomPopupMenuButton(
-            tooltip: 'Filter by Protocols',
-            key: const Key('show-filter-protocol'),
-            onSelected: widget.onSelected,
-            child: Padding(
-              padding: EdgeInsets.only(right: 3, left: 6),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  Text(
-                    widget.typeFilter.toUpperCase(),
-                    style: Theme.of(context).textTheme.bodyText1,
-                  ),
-                  SizedBox(width: 8),
-                  widget.typeFilter != ''
-                      ? InkWell(
-                          key: const Key('clear-filter-protocol'),
-                          onTap: () {
-                            widget.onSelected('');
-                            focusTextField(context);
-                          },
-                          child: Icon(
-                            Icons.close,
-                            color: Theme.of(context).colorScheme.onSurface,
-                          ),
-                        )
-                      : Icon(
+          child: Padding(
+            padding: EdgeInsets.only(right: 3, left: 6),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Text(
+                  widget.typeFilter.toUpperCase(),
+                  style: Theme.of(context).textTheme.bodyText1,
+                ),
+                SizedBox(width: 8),
+                widget.typeFilter != ''
+                    ? InkWell(
+                        key: const Key('clear-filter-protocol'),
+                        onTap: () {
+                          widget.onSelected('');
+                          focusTextField(context);
+                        },
+                        child: Icon(
+                          Icons.close,
+                          color: Theme.of(context).colorScheme.onSurface,
+                        ),
+                      )
+                    : InkWell(
+                        onTap: _showMenu,
+                        child: Icon(
                           Icons.filter_list,
                           color: Theme.of(context).colorScheme.onSurface,
                         ),
-                ],
-              ),
+                      ),
+              ],
             ),
-            itemBuilder: (_) => widget.allCoinsTypes
-                .map(
-                  (e) => menu.PopupMenuItem<String>(
-                    key: Key('filter-item-' + e),
-                    value: e,
-                    child: Text(
-                      _typesName[e] ?? '',
-                    ),
-                  ),
-                )
-                .toList(),
           ),
         )
       ],
     );
+  }
+
+  void _showMenu() {
+    final RenderBox button =
+        globalKey.currentContext.findRenderObject() as RenderBox;
+    final RenderBox overlay =
+        Navigator.of(context).overlay.context.findRenderObject() as RenderBox;
+    const Offset offset = Offset.zero;
+
+    final RelativeRect position = RelativeRect.fromRect(
+      Rect.fromPoints(
+        button.localToGlobal(offset, ancestor: overlay),
+        button.localToGlobal(button.size.bottomRight(Offset.zero) + offset,
+            ancestor: overlay),
+      ),
+      Offset.zero & overlay.size,
+    );
+
+    dialogBloc.dialog = showMenu(
+      context: context,
+      position: position,
+      items: _buildMenuItem(),
+    ).then((value) => dialogBloc.dialog = null);
+  }
+
+  List<PopupMenuEntry<String>> _buildMenuItem() {
+    return widget.allCoinsTypes.map((String protocolType) {
+      return PopupMenuItem<String>(
+        key: Key('filter-item-' + protocolType),
+        value: protocolType,
+        child: Text(
+          _typesName[protocolType] ?? '',
+        ),
+      );
+    }).toList();
   }
 }
