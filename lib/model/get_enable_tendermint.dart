9c685e08e1473cb0f19bb62db5dca9aa9b4f7b59
Temitope Ologunbaba <temitope@glade.ng>
2022-11-14 00:21:59 +0100

    create mmrpc enable model

---
+++ lib/model/get_enable_tendermint.dart
@@ -0,0 +1,49 @@
+import 'coin.dart';
+import 'get_active_coin.dart';
+
+class MmIrisEnable {
+  MmIrisEnable({
+    this.userpass,
+    this.method = 'enable_tendermint_token',
+    this.coin,
+  });
+
+  Coin coin;
+  String userpass;
+  String method;
+
+  Map<String, dynamic> toJson() => <String, dynamic>{
+        'mmrpc': '2.0',
+        'userpass': userpass,
+        'method': method,
+        'params': {
+          'ticker': coin.abbr,
+          'activation_params': {},
+        }
+      };
+}
+
+class MmCosmosEnable {
+  MmCosmosEnable({
+    this.userpass,
+    this.method = 'enable_tendermint_with_assets',
+    this.coin,
+    this.servers,
+  });
+
+  Coin coin;
+  String userpass;
+  String method;
+  List<Server> servers;
+
+  Map<String, dynamic> toJson() => <String, dynamic>{
+        'mmrpc': '2.0',
+        'userpass': userpass,
+        'method': method,
+        'params': {
+          'tokens_params': [],
+          'rpc_urls': servers.map((e) => e.url).toList(),
+          'ticker': coin.abbr,
+        }
+      };
+}
