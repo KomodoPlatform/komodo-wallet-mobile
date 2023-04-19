f463a34edbedc7d63cfd851d7970921b34aec4d2
CharlVS <charl@vanstaden.info>
2023-04-19 18:19:44 +0200

    Refactor auth state to use wallet profile
    Refactor auth state to use wallet profile instead of a user model from the source project for this feature.

--- lib/packages/authentication/bloc/authentication_state.dart
+++ lib/packages/authentication/bloc/authentication_state.dart
@@ -1,22 +1,27 @@
-part of 'authentication_bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:komodo_dex/packages/authentication/repository/authentication_repository.dart';
+import 'package:komodo_dex/packages/wallet_profiles/models/wallet_profile.dart';
 
 class AuthenticationState extends Equatable {
   final AuthenticationStatus status;
-  final User user;
+  final WalletProfile? walletProfile;
 
   const AuthenticationState._({
     this.status = AuthenticationStatus.unknown,
-    this.user = User.empty,
+    this.walletProfile,
   });
 
   const AuthenticationState.unknown() : this._();
 
-  const AuthenticationState.authenticated(User user)
-      : this._(status: AuthenticationStatus.authenticated, user: user);
+  const AuthenticationState.authenticated(WalletProfile walletProfile)
+      : this._(
+          status: AuthenticationStatus.authenticated,
+          walletProfile: walletProfile,
+        );
 
   const AuthenticationState.unauthenticated()
       : this._(status: AuthenticationStatus.unauthenticated);
 
   @override
-  List<Object> get props => [status, user];
+  List<Object?> get props => [status, walletProfile];
 }
