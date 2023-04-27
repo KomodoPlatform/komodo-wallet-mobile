313a2f3e88644cb43084bba0ef307b9074d4f052
CharlVS <charl@vanstaden.info>
2023-04-27 15:12:50 +0200

    Organise adex api to be package-compatible
    Move adex client to src folder and create index file so that adex api directory is compatible with a package directory for the future when the package is created.
    
    Create data models folder

---
+++ lib/atomicdex_api/atomicdex_api.dart
@@ -0,0 +1,7 @@
+import 'src/models/account/account.dart';
+import 'src/models/account/account_id.dart';
+import 'src/client.dart';
+
+export 'src/models/account/account.dart';
+export 'src/models/account/account_id.dart';
+export 'src/client.dart';
