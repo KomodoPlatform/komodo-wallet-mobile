0b25c304d7a8da4df949cac84af65220bba49ac9
naezith <naezith@gmail.com>
2023-06-27 12:42:19 +0300

    validation and cleanup for AccountAddressesApiHive

--- lib/packages/account_addresses/api/account_addresses_api_hive.dart
+++ lib/packages/account_addresses/api/account_addresses_api_hive.dart
@@ -8,11 +8,17 @@ class AccountAddressesApiHive implements AccountAddressesApiInterface {
 
   AccountAddressesApiHive() {
     Hive.registerAdapter(WalletAddressAdapter());
-    Hive.openBox<WalletAddress>('account_addresses').then((box) => _box = box);
+    Hive.openBox<WalletAddress>('account_addresses')
+        .then((box) => _box = box)
+        .onError((error, stackTrace) {
+      throw Exception('Error when opening the box: $error');
+    });
   }
 
   @override
   Future<void> create(WalletAddress walletAddress) async {
+    _validateWalletAddress(walletAddress);
+
     await _box.put(
         '${walletAddress.walletId}_${walletAddress.address}', walletAddress);
   }
@@ -28,6 +34,8 @@ class AccountAddressesApiHive implements AccountAddressesApiInterface {
     }
 
     if (updateFields != null) {
+      _validateWalletAddress(updateFields);
+
       final updatedWalletAddress = WalletAddress(
         walletId: walletId,
         address: address,
@@ -70,5 +78,22 @@ class AccountAddressesApiHive implements AccountAddressesApiInterface {
         .watch()
         .where((event) => event.key.startsWith(walletId))
         .map((event) => event.value);
+  }
+
+  Future<void> close() async {
+    await _box.close();
+  }
+
+  void _validateWalletAddress(WalletAddress walletAddress) {
+    if (walletAddress.walletId == null || walletAddress.walletId.isEmpty) {
+      throw ArgumentError('Wallet ID must not be empty');
+    }
+    if (walletAddress.address == null || walletAddress.address.isEmpty) {
+      throw ArgumentError('Address must not be empty');
+    }
+    if (walletAddress.availableBalance == null ||
+        walletAddress.availableBalance < 0) {
+      throw ArgumentError('Available balance must be non-negative');
+    }
   }
 }
