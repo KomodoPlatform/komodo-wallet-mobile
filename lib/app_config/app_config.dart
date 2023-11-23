c76eeb5c71d5f4b2c02457bc2b6462105f82fb61
naezith <naezith@gmail.com>
2023-11-23 15:43:23 +0300

    rebrand rpcPort to 7769

--- lib/app_config/app_config.dart
+++ lib/app_config/app_config.dart
@@ -236,5 +236,5 @@ class AppConfig {
 
   /// We're using different rpc ports for different wallet packages
   /// in order to allow multiple wallets to run simultaneously.
-  int get rpcPort => 7783;
+  int get rpcPort => 7769;
 }
