c91218cb91ebfef78483e93bd00f343b75a04034
Francois van Wyk <francoisvw@protonmail.com>
2023-12-12 11:47:42 +0100

    Add command line argument "-f/--force"
    Cleans existing coin asset files and redownloads assets

--- fetch_coin_assets.py
+++ fetch_coin_assets.py
@@ -4,6 +4,7 @@ import os
 import shutil
 from typing import Any
 import concurrent.futures
+import argparse
 
 
 COINS_URL = "https://raw.githubusercontent.com/KomodoPlatform/coins"
@@ -11,6 +12,33 @@ COINS_CI_PATH = "./coins_ci.json"
 COINS_PATH = "./assets/coins.json"
 COINS_CONFIG_PATH = "./assets/coins_config.json"
 COIN_ICONS_PATH = "./assets/coin-icons"
+ICON_DOWNLOAD_TIMEOUT_SECONDS = 10
+CONCURRENCY_LIMIT = 4
+
+
+def main() -> None:
+    parser = init_argparse()
+    args = parser.parse_args()
+
+    if args.force:
+        clean_coin_assets()
+
+    coin_configs_exist = os.path.exists(COINS_PATH) and os.path.exists(
+        COINS_CONFIG_PATH
+    )
+    coin_icons_exist = os.path.exists(COIN_ICONS_PATH)
+    if coin_configs_exist and coin_icons_exist:
+        print("Coin configs and icons already exist. Skipping download.")
+        return
+
+    coins_repo_commit = load_coins_commit()
+    download_coin_configs(coins_repo_commit)
+    coin_names = load_coin_names()
+
+    if not os.path.exists(COIN_ICONS_PATH):
+        os.mkdir(COIN_ICONS_PATH)
+
+    download_coin_icons(coins_repo_commit, coin_names)
 
 
 def download_coin_configs(coins_repo_commit: str) -> None:
@@ -47,16 +75,21 @@ def download_icon(coins_repo_commit: str, coin_name: str) -> None:
             f.write(icon)
     except Exception as e:
         print(f"Failed to download icon for {coin_name}: {e}")
+        raise e
 
 
 def download_coin_icons(
-    coins_repo_commit: str, coin_names: list[str], max_concurrency: int = 4
+    coins_repo_commit: str,
+    coin_names: list[str],
+    max_concurrency: int = CONCURRENCY_LIMIT,
+    timeout: int = ICON_DOWNLOAD_TIMEOUT_SECONDS,
 ) -> None:
     """
     Download the coin icons from the coins repo in parallel
     :param coins_repo_commit: commit id of the coins repo
     :param coin_names: list of coin names
     :param max_concurrency: maximum number of concurrent downloads
+    :param timeout: timeout for each download in seconds
     :return: None
     """
     with concurrent.futures.ThreadPoolExecutor(max_workers=max_concurrency) as executor:
@@ -65,13 +98,15 @@ def download_coin_icons(
             for coin_name in coin_names
         }
         for future in concurrent.futures.as_completed(futures):
-            try:
-                future.result()
-            except Exception as e:
-                print(f"Failed to download an icon: {e}")
+            # Exceptions that occur in the threads are allowed to propagate
+            # to the main thread, so that the program can exit with a non-zero
+            # exit code. This is useful for CI/CD pipelines.
+            future.result(timeout=timeout)
 
 
-def load_coin_names(coins_path: str = COINS_PATH) -> list[str]:
+def load_coin_names(
+    coins_path: str = COINS_PATH, coins_config_path: str = COINS_CONFIG_PATH
+) -> list[str]:
     """
     Load the coin names from the coins.json file
     by taking the first part of the `coin` field in each coin object
@@ -82,13 +117,14 @@ def load_coin_names(coins_path: str = COINS_PATH) -> list[str]:
     with open(coins_path, "r") as f:
         coins = json.load(f)
 
+    with open(coins_config_path, "r") as f:
+        coins_config = json.load(f)
+        coins += [coins_config[key] for key in coins_config.keys()]
+
     if len(coins) == 0:
         raise Exception("No coins found in coins.json")
 
-    coin_names = []
-    for coin in coins:
-        coin_name = coin["coin"].split("-")[0]
-        coin_names.append(coin_name.lower())
+    coin_names = [str(coin["coin"].split("-")[0]).lower() for coin in coins]
 
     return coin_names
 
@@ -108,11 +144,27 @@ def load_coins_commit(ci_path: str = COINS_CI_PATH) -> str:
     return ci["coins_repo_commit"]
 
 
-def main() -> None:
+def init_argparse() -> argparse.ArgumentParser:
+    """Initialize argparse
+
+    Returns:
+        argparse.ArgumentParser: parser
     """
-    Download the latest coin configs and icons.
-    Delete the old ones first (if they exist).
-    """
+    parser = argparse.ArgumentParser(
+        usage="%(prog)s [OPTION]...",
+        description="Download the latest coin configs and icons.",
+    )
+    parser.add_argument(
+        "-f",
+        "--force",
+        action="store_true",
+        help="Force download the latest coin configs and icons. NOTE: This will delete the existing files.",
+    )
+    return parser
+
+
+def clean_coin_assets() -> None:
+    """Remove existing coin config files and icons."""
     if os.path.exists(COINS_PATH):
         os.remove(COINS_PATH)
 
@@ -121,13 +173,6 @@ def main() -> None:
 
     if os.path.exists(COIN_ICONS_PATH):
         shutil.rmtree(COIN_ICONS_PATH, ignore_errors=True)
-
-    os.mkdir("./assets/coin-icons")
-
-    coins_repo_commit = load_coins_commit()
-    download_coin_configs(coins_repo_commit)
-    coin_names = load_coin_names()
-    download_coin_icons(coins_repo_commit, coin_names)
 
 
 if __name__ == "__main__":
