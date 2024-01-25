fbfb81a01c566a2225bfa858aec40f87bcc441d7
Francois <takenagain@users.noreply.github.com>
2024-01-25 11:50:23 +0100

    Rebrand colour scheme to Firo Branding

--- lib/drawer/drawer.dart
+++ lib/drawer/drawer.dart
@@ -57,8 +57,8 @@ class _AppDrawerState extends State<AppDrawer> {
                     begin: Alignment.centerRight,
                     stops: <double>[0.01, 1],
                     colors: <Color>[
-                      Color.fromRGBO(98, 90, 229, 1),
-                      Color.fromRGBO(45, 184, 240, 1),
+                      Color.fromRGBO(124, 22, 36, 1),
+                      Color.fromRGBO(186, 42, 69, 1),
                     ],
                   ),
                 ),
