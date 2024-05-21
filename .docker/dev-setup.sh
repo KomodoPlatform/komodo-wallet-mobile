d804885436c28d1a39e6b68eba52106f07f99561
Francois <takenagain@users.noreply.github.com>
2024-05-21 17:30:29 +0200

    Update devcontainer image and fix permission issues

---
+++ .docker/dev-setup.sh
@@ -0,0 +1,5 @@
+chmod u+rw,g+r komodo:komodo /home/komodo/workspace
+mkdir -p android/app/src/main/cpp/libs/armeabi-v7a
+mkdir -p android/app/src/main/cpp/libs/arm64-v8a
+/home/komodo/.venv/bin/pip install -r .docker/requirements.txt 
+/home/komodo/.venv/bin/python .docker/update_api.py
