cfd863c887fb2876c68008de897a807b9f32829e
CharlVS <77973576+CharlVS@users.noreply.github.com>
2024-05-22 13:37:57 +0200

    Fix script to use version tag
    Fix the script to use a version tag instead of a hash, as GH’s API does not support fetching a release tied to a specific commit. In the future, we can investigate if there’s an alternate way to verify/validate that the release build is indeed built from a given hash so we can change from a tag ref to a hash ref.

--- .docker/update_api.py
+++ .docker/update_api.py
@@ -14,8 +14,8 @@ from datetime import datetime
 
 class UpdateAPI():
     '''Updates the API module version for all or a specified platform.'''
-    def __init__(self, version=None, platform=None, force=False):
-        self.version = version
+    def __init__(self, tag=None, platform=None, force=False):
+        self.tag = tag
         self.platform = platform
         self.force = force
 
@@ -27,9 +27,9 @@ class UpdateAPI():
         with open(self.config_file, "r") as f:
             self.config = json.load(f)
         
-        # Use version from build_config.json if version is not specified
-        if not self.version:
-            self.version = self.config["api"]["release_version"]
+        # Use tag from build_config.json if tag is not specified
+        if not self.tag:
+            self.tag = self.config["api"]["release_tag"]
 
         # Get the platforms config
         self.platforms_config = self.config["api"]["platforms"]
@@ -47,27 +47,36 @@ class UpdateAPI():
         else:
             raise ValueError(f"Invalid platform: {platform}. Please select a valid platform from the following list: {', '.join(self.platforms_config.keys())}")
 
-    def get_release_assets(self, latest=False):
-        '''Fetches the assets of the release from the GitHub repository.'''
-        if latest:
+    def get_release_assets(self):
+        '''Fetches the assets of the specified or latest release from the GitHub repository.'''
+        if self.use_latest_release:
             api_url = self.github_repo.replace("https://github.com/", "https://api.github.com/repos/") + "/releases/latest"
         else:
-            api_url = self.github_repo.replace("https://github.com/", "https://api.github.com/repos/") + f"/commits/{self.version}/releases"
+            api_url = self.github_repo.replace("https://github.com/", "https://api.github.com/repos/") + "/releases"
         
         response = requests.get(api_url)
         response.raise_for_status()
-        return response.json()["assets"]
+        releases = response.json()
+
+        if self.use_latest_release:
+            return releases["assets"]
+        else:
+            for release in releases:
+                if self.tag == release["tag_name"]:
+                    return release["assets"]
+
+        raise ValueError(f"Could not find a release for tag '{self.tag}'")
 
     def get_zip_file_url(self, platform):
         '''Returns the URL of the zip file for the requested version / platform.'''
-        assets = self.get_release_assets(latest=self.use_latest_release)
+        assets = self.get_release_assets()
         search_parameters = self.platforms_config[platform]
         keywords = search_parameters["keywords"]
         for asset in assets:
             file_name = asset["name"]
-            if all(keyword in file_name for keyword in keywords) and (self.version in file_name if not self.use_latest_release else True):
+            if all(keyword in file_name for keyword in keywords) and (self.tag in file_name if not self.use_latest_release else True):
                 return asset["browser_download_url"]
-        raise ValueError(f"Could not find release zip file for version '{self.version}' on '{platform}' platform!")
+        raise ValueError(f"Could not find release zip file for tag '{self.tag}' on '{platform}' platform!")
 
     def download_api_file(self, platform):
         '''Downloads the API version zip file for a specific platform.'''
@@ -75,7 +84,7 @@ class UpdateAPI():
         zip_file_url = self.get_zip_file_url(platform)
 
         # Download the zip file
-        print(f"Downloading '{self.version}' API module for [{platform}]...")
+        print(f"Downloading '{self.tag}' API module for [{platform}]...")
         response = requests.get(zip_file_url, stream=True)
         response.raise_for_status()
 
@@ -102,7 +111,7 @@ class UpdateAPI():
         # Update the version information in the documentation
         updated_content = re.sub(
             r"(Current api module version is) `([^`]+)`",
-            f"\\1 `{self.version}`",
+            f"\\1 `{self.tag}`",
             content
         )
 
@@ -110,11 +119,11 @@ class UpdateAPI():
         with open(documentation_file, "w") as f:
             f.write(updated_content)
 
-        print(f"API version in documentation updated to {self.version}")
+        print(f"API version in documentation updated to {self.tag}")
 
     def update_api(self):
         '''Updates the API module.'''
-        version = self.config["api"]["release_version"]
+        tag = self.config["api"]["release_tag"]
         platforms = self.config["api"]["platforms"]
 
         # If a platform is specified, only update that platform
@@ -133,8 +142,8 @@ class UpdateAPI():
             if not self.force and os.path.exists(last_api_update_file):
                 with open(last_api_update_file, "r") as f:
                     last_api_update = json.load(f)
-                    if last_api_update.get("version") == version:
-                        print(f"{platform} API module is up to date ({version}).")
+                    if last_api_update.get("tag") == tag:
+                        print(f"{platform} API module is up to date ({tag}).")
                         is_outdated = False
 
             if is_outdated:
@@ -179,31 +188,31 @@ class UpdateAPI():
                 # Update .api_last_updated file
                 with open(last_api_update_file, "w") as f:
                     current_timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                    json.dump({"version": version, "timestamp": current_timestamp}, f)
+                    json.dump({"tag": tag, "timestamp": current_timestamp}, f)
     
-    def update_build_config_version(self):
-        '''Updates the API version in build_config.json.'''
-        self.config["api"]["release_version"] = self.version
+    def update_build_config_tag(self):
+        '''Updates the API tag in build_config.json.'''
+        self.config["api"]["release_tag"] = self.tag
 
         with open(self.config_file, "w") as f:
             json.dump(self.config, f, indent=4)
 
-        print(f"Version in build_config.json updated to {self.version}.")
+        print(f"Tag in build_config.json updated to {self.tag}.")
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Download API module file for specified version and platform.")
+    parser = argparse.ArgumentParser(description="Download API module file for specified tag and platform.")
     # Optional arguments
-    parser.add_argument("-a", "--api", help="version of the API module to download.", default=None)
+    parser.add_argument("-a", "--api", help="tag of the API module to download.", default=None)
     parser.add_argument("-p", "--platform", help="platform for which the API module should be downloaded.", default=None)
     parser.add_argument("--force", action="store_true", help="force update, ignoring .api_last_updated.", default=False)
     args = parser.parse_args()
 
     try:
         updateAPI = UpdateAPI(args.api, args.platform, args.force)
-        # Update the API version in build_config.json if the API version is specified
+        # Update the API tag in build_config.json if the API tag is specified
         if args.api:
-            updateAPI.update_build_config_version()
+            updateAPI.update_build_config_tag()
         updateAPI.update_api()
 
     except Exception as e:
