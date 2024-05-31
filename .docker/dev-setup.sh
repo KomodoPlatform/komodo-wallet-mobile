504e697d6d3b49264c2ba7f1aec03d0fffa0727d
Francois <takenagain@users.noreply.github.com>
2024-05-31 20:45:54 +0200

    Combine dockerfiles into `android-dev` for Github codespaces

--- .docker/dev-setup.sh
+++ .docker/dev-setup.sh
@@ -1,6 +1,11 @@
-chmod u+rw,g+r /home/komodo/workspace
-mkdir -p android/app/src/main/cpp/libs/armeabi-v7a
-mkdir -p android/app/src/main/cpp/libs/arm64-v8a
-/home/komodo/.venv/bin/pip install -r .docker/requirements.txt 
-/home/komodo/.venv/bin/python .docker/update_api.py
+sudo chmod -R u+rwx,g+rwx /home/komodo/workspace
+sudo chown -R /home/komodo/workspace
 flutter pub get
+
+curl -o assets/coins.json https://raw.githubusercontent.com/KomodoPlatform/coins/master/coins 
+curl -o assets/coins_config.json https://raw.githubusercontent.com/KomodoPlatform/coins/master/utils/coins_config.json 
+mkdir -p android/app/src/main/cpp/libs/armeabi-v7a 
+mkdir -p android/app/src/main/cpp/libs/arm64-v8a 
+
+cp /kdf/target/aarch64-linux-android/release/libmm2.a android/app/src/main/cpp/libs/arm64-v8a
+cp /kdf/target/armv7-linux-androideabi/release/libmm2.a android/app/src/main/cpp/libs/armeabi-v7a
