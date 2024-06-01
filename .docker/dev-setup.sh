6f33edee9890601c9fceb9bd112642fc35e4501e
Francois <takenagain@users.noreply.github.com>
2024-06-01 20:22:03 +0200

    Move API build to postAttach event of devcontainer
    API was built in image to reduce Codespaces startup time, but ended up increasing the already large image size

--- .docker/dev-setup.sh
+++ .docker/dev-setup.sh
@@ -1,5 +1,5 @@
 sudo chmod -R u+rwx,g+rwx /home/komodo/workspace
-sudo chown -R /home/komodo/workspace
+sudo chown -R komodo:komodo /home/komodo/workspace
 flutter pub get
 
 curl -o assets/coins.json https://raw.githubusercontent.com/KomodoPlatform/coins/master/coins 
@@ -7,5 +7,13 @@ curl -o assets/coins_config.json https://raw.githubusercontent.com/KomodoPlatfor
 mkdir -p android/app/src/main/cpp/libs/armeabi-v7a 
 mkdir -p android/app/src/main/cpp/libs/arm64-v8a 
 
-cp /kdf/target/aarch64-linux-android/release/libmm2.a android/app/src/main/cpp/libs/arm64-v8a
-cp /kdf/target/armv7-linux-androideabi/release/libmm2.a android/app/src/main/cpp/libs/armeabi-v7a
+cd /kdf 
+export PATH="$HOME/.cargo/bin:$PATH" 
+export PATH=$PATH:/android-ndk/bin
+CC_aarch64_linux_android=aarch64-linux-android21-clang CARGO_TARGET_AARCH64_LINUX_ANDROID_LINKER=aarch64-linux-android21-clang cargo rustc --target=aarch64-linux-android --lib --release --crate-type=staticlib --package mm2_bin_lib
+CC_armv7_linux_androideabi=armv7a-linux-androideabi21-clang CARGO_TARGET_ARMV7_LINUX_ANDROIDEABI_LINKER=armv7a-linux-androideabi21-clang cargo rustc --target=armv7-linux-androideabi --lib --release --crate-type=staticlib --package mm2_bin_lib
+mv target/aarch64-linux-android/release/libmm2lib.a target/aarch64-linux-android/release/libmm2.a 
+mv target/armv7-linux-androideabi/release/libmm2lib.a target/armv7-linux-androideabi/release/libmm2.a
+
+mv /kdf/target/aarch64-linux-android/release/libmm2.a /home/komodo/workspace/android/app/src/main/cpp/libs/arm64-v8a/libmm2.a
+mv /kdf/target/armv7-linux-androideabi/release/libmm2.a /home/komodo/workspace/android/app/src/main/cpp/libs/armeabi-v7a/libmm2.a
