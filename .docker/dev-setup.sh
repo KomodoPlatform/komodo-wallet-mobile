2181747221bab64e63fbd64a4e8379f5264d3efb
Francois <takenagain@users.noreply.github.com>
2024-06-03 21:31:21 +0200

    Fix cargo install in devcontainer/Codespaces

--- .docker/dev-setup.sh
+++ .docker/dev-setup.sh
@@ -1,9 +1,14 @@
-sudo chmod -R u+rwx,g+rwx /home/komodo/workspace
-sudo chown -R komodo:komodo /home/komodo/workspace
+#!/bin/bash
+
+set -e
+
+sudo git config core.fileMode false
+sudo chmod -R u+rwx /home/komodo
+sudo chown -R komodo:komodo /home/komodo
 flutter pub get
 
 curl -o assets/coins.json https://raw.githubusercontent.com/KomodoPlatform/coins/master/coins 
-curl -o assets/coins_config.json https://raw.githubusercontent.com/KomodoPlatform/coins/master/utils/coins_config.json 
+curl -o assets/coins_config_tcp.json https://raw.githubusercontent.com/KomodoPlatform/coins/master/utils/coins_config_tcp.json 
 mkdir -p android/app/src/main/cpp/libs/armeabi-v7a 
 mkdir -p android/app/src/main/cpp/libs/arm64-v8a 
 
