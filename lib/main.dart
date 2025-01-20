391589ef3e51c41e44e17cfa96514d06d9446fae
Francois <takenagain@users.noreply.github.com>
2025-01-20 16:40:17 +0200

    move debug mode dialog to CoinsPage
    fix issue with dialog pre-empting page load and causing the app to lock

--- lib/main.dart
+++ lib/main.dart
@@ -326,12 +326,6 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
 
     _initLanguage();
     lockService.initialize();
-
-    WidgetsBinding.instance.addPostFrameCallback((_) async {
-      if (kDebugMode && !await _hasAgreedNoTrading()) {
-        _showDebugModeDialog().ignore();
-      }
-    });
   }
 
   @override
@@ -602,43 +596,5 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
     } else {
       _scaffoldKey.currentState.openEndDrawer();
     }
-  }
-
-  // Method to show an alert dialog with an option to agree if the app is in
-  // debug mode stating that trading features may not be used for actual trading
-  // and that only test assets/networks may be used.
-  Future<void> _showDebugModeDialog() async {
-    await showDialog(
-      context: context,
-      barrierDismissible: false,
-      builder: (context) {
-        return AlertDialog(
-          title: const Text('Debug mode'),
-          content: const Text(
-            'This app is in debug mode. Trading features may not be used for '
-            'actual trading. Only test assets/networks may be used.',
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-                _saveAgreedState().ignore();
-              },
-              child: const Text('I agree'),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  Future<void> _saveAgreedState() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    prefs.setBool('wallet_only_agreed', true);
-  }
-
-  Future<bool> _hasAgreedNoTrading() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    return prefs.getBool('wallet_only_agreed') ?? false;
   }
 }
